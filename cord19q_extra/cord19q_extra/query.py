24ee5fba4c6ac0c00e90ad11a8bf0cb02e712147
Pierre-Alexandre Fonta <pierre-alexandre.fonta@epfl.ch>
2020-04-01 18:41:38 +0200

    cord19q_extra > 01.04.2020

--- cord19q_extra/cord19q_extra/query.py
+++ cord19q_extra/cord19q_extra/query.py
@@ -1,18 +1,75 @@
+from cord19q.models import Models
 from cord19q.query import Query
-from cord19q.tokenizer import Tokenizer
+
+from .models import CustomModels
 
 
 class CustomQuery(Query):
 
     @staticmethod
-    def search(embeddings, cur, query, topn):
+    def search(embeddings, cur, query, method: str, topn):
         results = []
 
-        # TODO
-        query = Tokenizer.tokenize(query)
-
-        for uid, score in embeddings.search(query, topn):
+        for uid, score in embeddings.search(query, method, topn):
             cur.execute("SELECT Article, Text FROM sections WHERE id = ?", [uid])
             results.append((uid, score) + cur.fetchone())
 
         return results
+
+    @staticmethod
+    def query(embeddings, db, query, method: str, topn):
+        # Default to 10 results if not specified
+        topn = topn if topn else 10
+
+        cur = db.cursor()
+
+        print(Query.render("#Query: %s" % query, theme="729.8953") + "\n")
+
+        # Query for best matches
+        results = CustomQuery.search(embeddings, cur, query, method, topn)
+
+        # Extract top sections as highlights
+        print(Query.render("# Highlights"))
+        for highlight in Query.highlights(results, int(topn / 5)):
+            print(Query.render("## - %s" % Query.text(highlight)))
+
+        print()
+
+        # Get results grouped by document
+        documents = Query.documents(results)
+
+        print(Query.render("# Articles") + "\n")
+
+        # Print each result, sorted by max score descending
+        for uid in sorted(documents, key=lambda k: sum([x[0] for x in documents[k]]),
+                          reverse=True):
+            cur.execute(
+                "SELECT Title, Authors, Published, Publication, LOE, Sample, Id, Reference from articles where id = ?",
+                [uid])
+            article = cur.fetchone()
+
+            print("Title: %s" % article[0])
+            print("Authors: %s" % Query.authors(article[1]))
+            print("Published: %s" % Query.date(article[2]))
+            print("Publication: %s" % article[3])
+            print("Level of Evidence: %s" % Query.loe(article[4]))
+            print("Sample: %s" % article[5])
+            print("Id: %s" % article[6])
+            print("Reference: %s" % article[7])
+
+            # Print top matches
+            for score, text in documents[uid]:
+                print(Query.render("## - (%.4f): %s" % (score, text), html=False))
+
+            print()
+
+    @staticmethod
+    def run(query, method: str, topn=None, path=None):
+        # Load model
+        embeddings, db = CustomModels.load(path)
+
+        # Query the database
+        CustomQuery.query(embeddings, db, query, method, topn)
+
+        # Free resources
+        Models.close(db)
