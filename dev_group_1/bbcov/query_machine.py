6770adad869808138f20f6e652a87aa40099fe69
Jan Krepl <jankrepl@yahoo.com>
2020-04-02 14:32:33 +0200

    Implement exclusion

--- dev_group_1/bbcov/query_machine.py
+++ dev_group_1/bbcov/query_machine.py
@@ -1,26 +1,32 @@
+import numpy as np
 import pandas as pd
 from sklearn.metrics import pairwise_distances
 
 
 class QueryMachine:
-    """
+    """Class used for querying from the embedding database.
+
+    Parameters
+    ----------
+    embedder : bbcov.Embedder
+
+    df_docs : pandas.DataFrame
+        The rows represent different documents. The columns are metadata.
+
+    text_column_name : str
+        The name of the column of `df_docs` to use for embeddings.
+
+    metric : str or callable
+        If string then it should be a valid metric parameter for
+        `sklearn.metrics.pairwise_distances`
+
     Attributes
     ----------
     doc_embeddings : pandas.DataFrame
+        Rows represent different
     """
 
     def __init__(self, embedder, df_docs, text_column_name, metric='cosine'):
-        """
-        Parameters
-        ----------
-        embedder : bbcov.Embedder
-        df_docs : pandas.DataFrame
-        text_column_name : str
-        metric : str or callable
-            If string then it should be a valid metric parameter for
-            `sklearn.metrics.pairwise_distances`
-        """
-
         self.df_docs = df_docs
         self.text_column_name = text_column_name
         self.embedder = embedder
@@ -40,7 +46,7 @@ class QueryMachine:
             doc_embeddings, metric=self.metric)
         return distances
 
-    def get_top_k(self, query, embeddings, k=3):
+    def get_top_k(self, query, embeddings, k=3, exclusion_query=None):
         """
 
         Parameters
@@ -51,6 +57,9 @@ class QueryMachine:
             An array of shape (n_documents, d_embedding)
         df_mask : array_like or None
 
+        exclusion_query : str or None
+            Exclusion query. If None then not considered.
+
         Returns
         -------
         top_k : pandas.Index
@@ -58,15 +67,29 @@ class QueryMachine:
         top_k_distances : list_like
             Distance of the top k matches to the query string
         """
+        alpha = 0.5
 
         query_embedding = self.embedder(query)
+
         distances = self.get_distances(
             query_embedding,
             embeddings.values)
         distances = distances[0]
 
-        top_k = distances.argsort()[:k]
-        top_k_distances = distances[top_k]
+        if exclusion_query is not None:
+            exclusion_embedding = self.embedder(exclusion_query)
+
+            exclusion_distances = self.get_distances(
+                exclusion_embedding,
+                embeddings.values)
+            exclusion_distances = exclusion_distances[0]
+        else:
+            exclusion_distances = np.zeros_like(distances)
+
+        overall_distances = (1 - alpha) * distances - alpha * exclusion_distances
+
+        top_k = overall_distances.argsort()[:k]
+        top_k_distances = overall_distances[top_k]
 
         return embeddings.index[top_k], top_k_distances
 
@@ -91,7 +114,7 @@ class QueryMachine:
 
         return most_relevant_paragraphs, paragraph_distances
 
-    def print_query(self, query, k=3, df_mask=None):
+    def print_query(self, query, k=3, df_mask=None, exclusion_query=None):
         """
 
         Parameters
@@ -101,6 +124,9 @@ class QueryMachine:
         df_mask : pandas.Series
             A boolean-valued pandas Series used to mask the original
             documents dataframe.
+
+        exclusion_query : str or None
+            Exclusion query. If None then not considered.
 
         """
 
@@ -112,7 +138,9 @@ class QueryMachine:
         top_k, top_k_distances = self.get_top_k(
             query,
             filtered_embeddings,
-            k=k)
+            k=k,
+            exclusion_query=exclusion_query)
+
         paragraphs, paragraph_distances = self.get_most_relevant_paragraphs(query, top_k)
         for i in range(len(top_k)):
             if i > 0:
