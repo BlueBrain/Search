4df5e8e553520afa71f6749a71eecaa59a3ee5e3
Stanislav Schmidt <stanislav.schmidt@epfl.ch>
2020-04-20 14:23:53 +0200

    Load JSON files on the fly
    Also:
    - Clean up data.py and add logging messages
    - Clean up models.py and add logging messages
    - Clean up widget.py

--- src/bbsearch/models.py
+++ src/bbsearch/models.py
@@ -19,40 +19,42 @@ class AllModels:
         self.assets_path = all_data.assets_path
         self.embeddings_path = all_data.embeddings_path
 
-        # Load USE
+        logger.info("Loading USE...")
         self.use_version = 5
         use_url = "https://tfhub.dev/google/universal-sentence-encoder-large"
         self.use = hub.load(f"{use_url}/{self.use_version}")
 
-        # Load SBERT
+        logger.info("Load SBERT...")
         self.sbert = SentenceTransformer('bert-base-nli-mean-tokens')
 
-        # Load BioSentVec
+        logger.info("Load BioSentVec...")
         self.bsv = sent2vec.Sent2vecModel()
         self.bsv.load_model(str(self.assets_path / 'BioSentVec_PubMed_MIMICIII-bigram_d700.bin'))
         self.bsv_stopwords = set(stopwords.words('english'))
 
-        # Load SBioBERT
+        logger.info("Load SBioBERT...")
         self.sbiobert = SBioBERT()
 
-        # Synonyms
+        logger.info("Processing synonyms...")
         self.synonyms_dict = dict()
         with open(self.assets_path / 'synonyms_list.txt', 'r', encoding='utf-8-sig') as f:
-            for l in [l_.strip().lower() for l_ in f]:
-                if l:
-                    w = [l_.strip() for l_ in l.split('=')]
-                    self.synonyms_dict[w[0]] = w[1:]
+            for line in f:
+                line = line.strip().lower()
+                if len(line) > 0:
+                    words = [word.strip() for word in line.split('=')]
+                    self.synonyms_dict[words[0]] = words[1:]
 
         del self.synonyms_dict['sars']
 
         self.synonyms_index = {x.lower(): k.lower()
                                for k, v in self.synonyms_dict.items() for x in v}
 
-        # Load embeddings from disk
+        logger.info("Loading sentence embeddings from disk...")
         self.EMBEDDINGS_NAMES = ['USE', 'SBERT', 'BSV', 'SBIOBERT']
 
         self.embeddings = dict()
         for embeddings_name in self.EMBEDDINGS_NAMES:
+            logger.info(f"> {embeddings_name}")
             current_embedding_name = f'{embeddings_name}_sentence_embeddings.npz'
             current_embedding_path = self.embeddings_path / current_embedding_name
             current_embedding = np.load(current_embedding_path)[embeddings_name]
@@ -60,6 +62,7 @@ class AllModels:
 
         self.embeddings_syns = dict()
         for embeddings_name in self.EMBEDDINGS_NAMES:
+            logger.info(f"> {embeddings_name}, merged synonyms")
             current_embedding_name = f'{embeddings_name}_sentence_embeddings_merged_synonyms.npz'
             current_embedding_path = self.embeddings_path / current_embedding_name
             current_embedding = np.load(current_embedding_path)[embeddings_name]
@@ -105,5 +108,5 @@ class AllModels:
             return embedding_model.embed_sentences(preprocessed)
 
         else:
-            raise NotImplementedError(f'Embedding {repr(embedding_name)} not '
-                                      f'available!')
+            raise NotImplementedError(
+                f'Embedding {repr(embedding_name)} not available!')
