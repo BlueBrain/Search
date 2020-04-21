70ec55055249ef1512076905abf921c3bce78dd3
Jan Krepl <jankrepl@yahoo.com>
2020-04-21 14:53:58 +0200

    Create fake sentences table

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,10 +1,15 @@
+"""Configuration of pytest."""
 from pathlib import Path
 import sqlite3
 
 import numpy as np
+import pandas as pd
 import pytest
 
 ROOT_PATH = Path(__file__).resolve().parent.parent  # root of the repository
+N_SENTENCES_PER_SECTION = 3
+N_SECTIONS_PER_ARTICLE = 2
+EMBEDDING_SIZE = 2
 
 
 @pytest.fixture(scope='session')
@@ -30,14 +35,91 @@ def cursor(cnxn, jsons_path, metadata_path):
     """Database object (sqlite)."""
     cursor = cnxn.cursor()
     # create
-    stmt_create_articles = """CREATE TABLE articles (article_id TEXT PRIMARY KEY, publisher TEXT)"""
-    stmt_create_sentences = """CREATE TABLE sentences (sentence_id INTEGER PRIMARY KEY, text TEXT)"""
+
+    articles_schema = {'article_id': 'TEXT PRIMARY KEY',
+                       'publisher': 'TEXT',
+                       'title': 'TEXT',
+                       'doi': 'TEXT',
+                       'pmc_id': 'TEXT',
+                       'pm_id': 'INTEGER',
+                       'licence': 'TEXT',
+                       'abstract': 'TEXT',
+                       'date': 'DATETIME',
+                       'authors': 'TEXT',
+                       'journal': 'TEXT',
+                       'microsoft_id': 'INTEGER',
+                       'covidence_id': 'TEXT',
+                       'has_pdf_parse': 'BOOLEAN',
+                       'has_pmc_xml_parse': 'BOOLEAN',
+                       'has_covid19_tag': 'BOOLEAN DEFAULT False',
+                       'fulltext_directory': 'TEXT',
+                       'url': 'TEXT'}
+
+    article_id_2_sha_schema = {'article_id': 'TEXT',
+                               'sha': 'TEXT'}
+
+    sentences_schema = {'sentence_id': 'INTEGER PRIMARY KEY',
+                        'sha': 'TEXT',
+                        'section_name': 'TEXT',
+                        'text': 'TEXT',
+                        'FOREIGN': 'KEY(sha) REFERENCES article_id_2_sha(sha)'}
+
+    stmt_create_articles = """CREATE TABLE articles ({})""".format(
+        ', '.join(['{} {}'.format(k, v) for k, v in articles_schema.items()]))
+
+    stmt_create_id_2_sha = """CREATE TABLE article_id_2_sha ({})""".format(
+        ', '.join(['{} {}'.format(k, v) for k, v in article_id_2_sha_schema.items()]))
+
+    stmt_create_sentences = """CREATE TABLE sentences ({})""".format(
+        ', '.join(['{} {}'.format(k, v) for k, v in sentences_schema.items()]))
+
     cursor.execute(stmt_create_articles)
+    cursor.execute(stmt_create_id_2_sha)
     cursor.execute(stmt_create_sentences)
 
     # Populate
-    cursor.executemany("INSERT INTO sentences (sentence_id, text) values (?, ?)",
-                       ((i, 'whatever') for i in range(2000)))
+    name_mapping = {
+        'cord_uid': 'article_id',
+        'sha': 'sha',
+        'source_x': 'publisher',
+        'title': 'title',
+        'doi': 'doi',
+        'pmcid': 'pmc_id',
+        'pubmed_id': 'pm_id',
+        'license': 'licence',
+        'abstract': 'abstract',
+        'publish_time': 'date',
+        'authors': 'authors',
+        'journal': 'journal',
+        'Microsoft Academic Paper ID': 'microsoft_id',
+        'WHO #Covidence': 'covidence_id',
+        'has_pdf_parse': 'has_pdf_parse',
+        'has_pmc_xml_parse': 'has_pmc_xml_parse',
+        'full_text_file': 'fulltext_directory',
+        'url': 'url'
+    }
+
+    metadata_df = pd.read_csv(str(metadata_path)).rename(columns=name_mapping).set_index('article_id')
+
+    article_id_2_content = metadata_df['sha']
+    article_id_2_content.to_sql(name='article_id_2_sha', con=cnxn, index=True, if_exists='append')
+
+    articles_content = metadata_df.drop(columns=['sha'])
+    articles_content.to_sql(name='articles', con=cnxn, index=True, if_exists='append')
+
+    temp = []
+    for sha in article_id_2_content[article_id_2_content.notna()].unique():
+        for sec_ix in range(N_SECTIONS_PER_ARTICLE):
+            for sen_ix in range(N_SENTENCES_PER_SECTION):
+                s = pd.Series({'text': 'I am a sentence {} in section {} in article {}'.format(sen_ix, sec_ix, sha),
+                               'section_name': 'section_{}'.format(sec_ix),
+                               'sha': sha
+                               })
+                temp.append(s)
+
+    sentences_content = pd.DataFrame(temp)
+    sentences_content.index.name = 'sentence_id'
+    sentences_content.to_sql(name='sentences', con=cnxn, index=True, if_exists='append')
 
     yield cursor
 
@@ -64,16 +146,20 @@ def metadata_path():
 
 @pytest.fixture(scope='session')
 def embeddings_path(tmp_path_factory, cursor):
-    """Path to a directory where emebddings stored."""
+    """Path to a directory where embeddings stored."""
     random_state = 3
     np.random.seed(random_state)
     models = ['BERT', 'BIOBERT']
 
     n_sentences = cursor.execute('SELECT COUNT(*) FROM sentences').fetchone()[0]
     embeddings_path = tmp_path_factory.mktemp('embeddings', numbered=False)
+
     for model in models:
         model_path = embeddings_path / model / '{}.npy'.format(model)
         model_path.parent.mkdir(parents=True)
-        np.save(str(model_path), np.random.rand(n_sentences, 2))
+        a = np.concatenate([np.arange(n_sentences).reshape(-1, 1), np.random.random((n_sentences, EMBEDDING_SIZE))],
+                           axis=1)
+
+        np.save(str(model_path), a)
 
     return embeddings_path
--- tests/test_fixtures.py
+++ tests/test_fixtures.py
@@ -1,35 +1,52 @@
-"""Collection of tests that make sure that fixtures are set up correctly."""
+"""Collection of tests that make sure that fixtures are set up correctly.
+
+Notes
+-----
+The internals of fixtures might vary based on how conftest.py sets them up. The goal of these
+tests is to run simple sanity checks rather than detailed bookkeeping.
+
+"""
+from sqlite3 import OperationalError
 
 import numpy as np
 import pandas as pd
+import pytest
 
 
-def test_sentences(cursor):
-    """Make sure sentences table setup correctly."""
-    all = cursor.execute('SELECT * FROM sentences').fetchall()
+def test_database(cursor):
+    """Make sure database tables setup correctly."""
+    for table_name in ['articles', 'article_id_2_sha', 'sentences']:
+        res = cursor.execute('SELECT * FROM {}'.format(table_name)).fetchall()
 
-    assert len(all) == 2000
+        assert len(res) > 0
+
+    with pytest.raises(OperationalError):
+        cursor.execute('SELECT * FROM fake_table').fetchall()
 
 
-def test_embeddings(embeddings_path):
+def test_embeddings(embeddings_path, cursor):
     """Make sure all sentences are embedded."""
+    n_sentences = cursor.execute('SELECT COUNT(*) FROM sentences').fetchone()[0]
+
     for p in embeddings_path.iterdir():
         model_path = p / '{}.npy'.format(p.stem)
 
         a = np.load(str(model_path))
 
-        assert a.shape == (2000, 2)
+        assert isinstance(a, np.ndarray)
+        assert a.shape[0] == n_sentences
+        assert a.shape[1] > 0
 
 
 def test_metadata(metadata_path):
     """Make sure all metadata csv is correct"""
     df = pd.read_csv(str(metadata_path))
 
-    assert len(df) == 11
+    assert len(df) > 0
 
 
 def test_jsons(jsons_path):
     """Make sure all jsons are present."""
     n_json_files = len(list(jsons_path.glob('**/*json')))
 
-    assert n_json_files == 17
+    assert n_json_files > 0
