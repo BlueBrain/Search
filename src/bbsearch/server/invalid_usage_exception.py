afd70eefc3bab05f5d70ff4e00f0866fd6bdcf42
Stanislav Schmidt <stanislav.schmidt@epfl.ch>
2020-04-24 17:18:24 +0200

    Add docstring to InvalidUsage

--- src/bbsearch/server/invalid_usage_exception.py
+++ src/bbsearch/server/invalid_usage_exception.py
@@ -1,4 +1,9 @@
 class InvalidUsage(Exception):
+    """An exception used in the REST API server
+
+    The code was largely copied from
+    https://flask.palletsprojects.com/en/1.1.x/patterns/apierrors/
+    """
 
     def __init__(self, message, status_code=None):
         Exception.__init__(self)
