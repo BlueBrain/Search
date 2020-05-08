0d754d41a18fc8d6c6d45c6be382fd14220c9eca
Emilie Delattre <emilie.delattre@epfl.ch>
2020-05-08 16:15:36 +0200

    Remove entrypoints

--- setup.py
+++ setup.py
@@ -5,20 +5,14 @@ VERSION = '0.1'
 description = "Blue Brain Search"
 
 install_requires = [
-    'absl-py',
-    'Flask',
-    'nltk',
     'numpy',
     'pandas',
-    'requests',
     'scikit-learn',
     'scipy',
-    'sent2vec-prebuilt',
-    'sentence-transformers',
+    'torch',
+    'Flask',
+    'requests',
     'spacy',
-    'tensorflow>=2.0.0a0',
-    'tensorflow_hub',
-    'torch'
 ]
 
 setup_requires = ['pytest-runner']
@@ -45,8 +39,6 @@ setup(
     entry_points={
         "console_scripts": [
             "embedding_server=bbsearch.server.embedding_server_entrypoint:main",
-            "create_database=bbsearch.create_database_entrypoint:main",
-            "save_embeddings=bbsearch.save_embeddings_entrypoint:main"
         ]
     }
 )
