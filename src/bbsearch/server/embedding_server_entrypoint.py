7dfb5ba946ce820e7655e498ba7181dce29b45c0
Emilie Delattre <emilie.delattre@epfl.ch>
2020-05-14 16:42:53 +0200

    Change the EmbeddingModel calls in the server part

--- src/bbsearch/server/embedding_server_entrypoint.py
+++ src/bbsearch/server/embedding_server_entrypoint.py
@@ -1,11 +1,11 @@
 import argparse
 import logging
-import os
 import pathlib
 
 from flask import Flask
 
 from .embedding_server import EmbeddingServer
+from bbsearch.embedding_models import USE, SBERT, SBioBERT, BSV
 
 
 logger = logging.getLogger(__name__)
@@ -19,26 +19,28 @@ parser.add_argument("--port",
                     default=8080,
                     type=int,
                     help="The server port")
+parser.add_argument("--bsv_checkpoints",
+                    default='/raid/covid_data/assets/BioSentVec_PubMed_MIMICIII-bigram_d700.bin',
+                    type=str,
+                    help="Path to file containing the checkpoints for the BSV model.")
 args = parser.parse_args()
 
 
 def main():
-    # Check the assets_path
-    if "ASSETS_PATH" in os.environ:
-        assets_path = pathlib.Path(os.environ["ASSETS_PATH"])
-        if not assets_path.exists():
-            raise ValueError("ASSETS_PATH does not represent a valid path.")
-        # Create Server app
-        app = Flask("BBSearch Embedding Server")
-        EmbeddingServer(app, assets_path)
-        app.run(
-            host=args.host,
-            port=args.port,
-            threaded=True,
-            debug=True,
-        )
-    else:
-        raise ValueError("Environmental variable ASSETS_PATH not found.")
+    embedding_models = {'USE': USE(),
+                        'SBERT': SBERT(),
+                        'BSV': BSV(checkpoint_model_path=pathlib.Path(args.bsv_checkpoints)),
+                        'SBioBERT': SBioBERT()}
+
+    # Create Server app
+    app = Flask("BBSearch Embedding Server")
+    EmbeddingServer(app, embedding_models)
+    app.run(
+        host=args.host,
+        port=args.port,
+        threaded=True,
+        debug=True,
+    )
 
 
 if __name__ == "__main__":
