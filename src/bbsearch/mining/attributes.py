1eada5c163a8346c649f2eee9da0f47cfe29e560
Stanislav Schmidt <stanislav.schmidt@epfl.ch>
2020-05-19 15:23:01 +0200

    Migrate more functions from notebook

--- src/bbsearch/mining/attributes.py
+++ src/bbsearch/mining/attributes.py
@@ -3,8 +3,11 @@ import json
 import logging
 import warnings
 
+import pandas as pd
 import requests
 from IPython.display import HTML
+
+from .entity import find_entities
 
 
 logger = logging.getLogger(__name__)
@@ -12,13 +15,15 @@ logger = logging.getLogger(__name__)
 
 class AttributeExtractor:
 
-    def __init__(self, core_nlp_url, grobid_quantities_url):
-        logger.debug("{} init".format(self.__class__.__name__))
-        logger.debug("CoreNLP URL: {}".format(core_nlp_url))
-        logger.debug("Grobid Quantities URL: {}".format(grobid_quantities_url))
+    def __init__(self, core_nlp_url, grobid_quantities_url, ee_model):
+        logger.debug(f"{self.__class__.__name__} init")
+        logger.debug(f"CoreNLP URL: {core_nlp_url}")
+        logger.debug(f"Grobid Quantities URL: {grobid_quantities_url}")
+        logger.debug(f"Entity Extraction Model:\n{ee_model.meta}")
 
         self.core_nlp_url = core_nlp_url
         self.grobid_quantities_url = grobid_quantities_url
+        self.ee_model = ee_model
 
     @staticmethod
     def get_quantity_type(quantity):
@@ -33,22 +38,10 @@ class AttributeExtractor:
         return quantity_type
 
     def get_measurement_type(self, measurement):
-        potential_quantity_keys = [
-            'quantity', 'quantityMost', 'quantityLeast',
-            'quantities', 'quantityBase', 'quantityRange']
-        found_keys = filter(lambda key: key in measurement,
-                            potential_quantity_keys)
-        key = next(iter(found_keys))
-
-        if key == 'quantities':
-            quantity_types = [self.get_quantity_type(quantity) for quantity in
-                              measurement['quantities']]
-            assert len(set(quantity_types)) == 1
-            quantity_type = quantity_types[0]
-        else:
-            quantity_type = self.get_quantity_type(measurement[key])
-
-        return quantity_type
+        quantity_types = [self.get_quantity_type(quantity)
+                          for quantity in self.iter_quantities(measurement)]
+        assert len(set(quantity_types)) == 1
+        return quantity_types[0]
 
     def get_all_measurement_types(self, measurements):
         all_types = [self.get_measurement_type(m) for m in measurements]
@@ -100,8 +93,6 @@ class AttributeExtractor:
         </style>
         """
 
-        annotations = []
-
         def annotate_quantity(quantity):
             annotations = []
             start = quantity['offsetStart']
@@ -119,25 +110,10 @@ class AttributeExtractor:
 
             return annotations
 
+        annotations = []
         for measurement in measurements:
-            if 'quantity' in measurement:
-                annotations += annotate_quantity(measurement['quantity'])
-            elif 'quantities' in measurement:
-                for quantity in measurement['quantities']:
-                    annotations += annotate_quantity(quantity)
-            elif 'quantityMost' in measurement or 'quantityLeast' in measurement:
-                if 'quantityLeast' in measurement:
-                    annotations += annotate_quantity(measurement['quantityLeast'])
-                if 'quantityMost' in measurement:
-                    annotations += annotate_quantity(measurement['quantityMost'])
-            elif 'quantityBase' in measurement or 'quantityRange' in measurement:
-                if 'quantityBase' in measurement:
-                    annotations += annotate_quantity(measurement['quantityBase'])
-                if 'quantityRange' in measurement:
-                    annotations += annotate_quantity(measurement['quantityRange'])
-            else:
-                warnings.warn("no quantity in measurement")
-                print(measurement)
+            annotations += [annotate_quantity(quantity)
+                            for quantity in self.iter_quantities(measurement)]
 
         sorted(annotations, key=lambda x: x[0])
         annotated_text = ''
@@ -149,3 +125,166 @@ class AttributeExtractor:
         html = css_styles + f"<div class=\"fixedWidth\">" + annotated_text + "</div>"
 
         return HTML(html)
+
+    @staticmethod
+    def get_overlapping_token_ids(start, end, tokens):
+        ids = []
+        for token in tokens:
+            start_inside = start <= token['characterOffsetBegin'] < end
+            end_inside = start < token['characterOffsetEnd'] <= end
+            if start_inside or end_inside:
+                ids.append(token['index'])
+
+        return ids
+
+    @staticmethod
+    def iter_quantities(measurement):
+        if 'quantity' in measurement:
+            yield measurement['quantity']
+        elif 'quantities' in measurement:
+            yield from measurement['quantities']
+        elif 'quantityMost' in measurement or 'quantityLeast' in measurement:
+            if 'quantityLeast' in measurement:
+                yield measurement['quantityLeast']
+            if 'quantityMost' in measurement:
+                yield measurement['quantityMost']
+        elif 'quantityBase' in measurement or 'quantityRange' in measurement:
+            if 'quantityBase' in measurement:
+                yield measurement['quantityBase']
+            if 'quantityRange' in measurement:
+                yield measurement['quantityRange']
+        else:
+            warnings.warn("no quantity in measurement")
+            return
+
+    def get_quantity_tokens(self, quantity, tokens):
+        value_start = quantity["offsetStart"]
+        value_end = quantity["offsetEnd"]
+        ids = self.get_overlapping_token_ids(value_start, value_end, tokens)
+        if "rawUnit" in quantity:
+            unit_start = quantity["rawUnit"]["offsetStart"]
+            unit_end = quantity["rawUnit"]["offsetEnd"]
+            ids += self.get_overlapping_token_ids(unit_start, unit_end, tokens)
+
+        return ids
+
+    def get_measurement_tokens(self, measurement, tokens):
+        ids = []
+
+        for quantity in self.iter_quantities(measurement):
+            ids += self.get_quantity_tokens(quantity, tokens)
+
+        return ids
+
+    def get_entity_tokens(self, entity, tokens):
+        return self.get_overlapping_token_ids(
+            entity.start_char,
+            entity.end_char,
+            tokens)
+
+    @staticmethod
+    def find_compound_parents(dependencies, tokens_d, token_idx):
+        parents = []
+        for link in dependencies:
+            if link['dependent'] == token_idx and link['dep'] == "compound":
+                parents.append(link['governor'])
+
+        return parents
+
+    @staticmethod
+    def iter_parents(dependencies, token_idx):
+        """
+        Seems each node has at most one parent (verify!)
+
+        It seems parent=0 means no parent
+        """
+
+        for link in dependencies:
+            if link['dependent'] == token_idx:
+                parent = link['governor']
+                if parent != 0:
+                    yield link['governor']
+
+    def find_nn_parents(self, dependencies, tokens_d, token_idx):
+        """
+        Ascent the dependency tree until find a parent
+        of type "NN". Do this for all parents. If, as
+        it seems, each node has at most one parent, then
+        the results will be either one index or no indices.
+        """
+
+        def get_nn(idx):
+            if tokens_d[idx]['pos'] == 'NN':
+                return [idx]
+            else:
+                nn_parents = []
+                for new_idx in self.iter_parents(dependencies, idx):
+                    nn_parents += get_nn(new_idx)
+                return nn_parents
+
+        results = []
+        for parent_idx in self.iter_parents(dependencies, token_idx):
+            results += get_nn(parent_idx)
+
+        return results
+
+    def find_all_parents(self, dependencies, tokens_d, tokens, parent_fn=None):
+        if parent_fn is None:
+            parent_fn = self.find_nn_parents
+
+        parent_ids = []
+
+        for token_idx in tokens:
+            parent_ids += parent_fn(dependencies, tokens_d, token_idx)
+
+        return parent_ids
+
+    def extract_attributes(self, text):
+        # NER
+        doc = find_entities(text, self.ee_model)
+        sent = list(doc.sents)[0]
+        detected_entities = [ent for ent in sent.ents]
+        logging.info("{} entities detected: {}".format(len(detected_entities), detected_entities))
+
+        # Grobid Quantities
+        measurements = self.get_grobid_measurements(text)
+        logging.info("{} measurements detected".format(len(measurements)))
+
+        # CoreNLP
+        logging.info("Sending CoreNLP query...")
+        try:
+            response = requests.post(self.core_nlp_url, data=text)
+            assert response.status_code == 200
+            response_json = json.loads(response.text)
+        except:
+            response_json = {'sentences': []}
+        logging.info("CoreNLP found {} sentences".format(len(response_json['sentences'])))
+
+        # Analysis
+        columns = ["entity", "entity_type", "attribute"]
+        rows = []
+
+        for entity in detected_entities:
+            for measurement in measurements:
+                for corenlp_sentence in response_json['sentences']:
+                    tokens = corenlp_sentence['tokens']
+                    dependencies = corenlp_sentence['basicDependencies']
+                    tokens_d = {token['index']: token for token in tokens}
+
+                    measurement_ids = self.get_measurement_tokens(measurement, tokens)
+                    ne_ids = self.get_entity_tokens(entity, tokens)
+
+                    measurement_parents = self.find_all_parents(dependencies, tokens_d, measurement_ids)
+                    ne_parents = self.find_all_parents(dependencies, tokens_d, ne_ids)
+
+                    if len(set(measurement_parents) & set(ne_parents)) > 0:
+                        row = {
+                            "entity": entity.text,
+                            "entity_type": entity.label_,
+                            "attribute": measurement
+                        }
+                        rows.append(row)
+
+        df_attributes = pd.DataFrame(rows, columns=columns)
+
+        return df_attributes
