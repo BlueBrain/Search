b75c1725d4f6b511ff8c513be9ddf8ac41f1fbbb
Stanislav Schmidt <stanislav.schmidt@epfl.ch>
2020-05-25 18:53:16 +0200

    Add new widget classes to __init__.py

--- src/bbsearch/mining/__init__.py
+++ src/bbsearch/mining/__init__.py
@@ -1,12 +1,17 @@
 """Subpackage for text mining."""
 
-from .attributes import AttributeExtractor
+from .attributes import (
+    AttributeExtractor,
+    AttributeAnnotationTab,
+    TextCollectionWidget)
 from .entity import find_entities
 from .pipeline import TextMiningPipeline
 from .relation import ChemProt, REModel, StartWithTheSameLetter, annotate
 
 __all__ = [
     'AttributeExtractor',
+    'AttributeAnnotationTab',
+    'TextCollectionWidget',
     'ChemProt',
     'REModel',
     'StartWithTheSameLetter',
