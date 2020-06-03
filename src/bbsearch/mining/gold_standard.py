21d1d8b09345ded3b37d815b89035d1f973d26a6
Emilie Delattre <emilie.delattre@epfl.ch>
2020-06-03 14:15:29 +0200

    Correct indent in create_dataset

--- src/bbsearch/mining/gold_standard.py
+++ src/bbsearch/mining/gold_standard.py
@@ -62,10 +62,10 @@ def create_baseline_dataset(db, words_to_find, entity_to_type, max_iter=1000000)
         if index % 100000 == 0:
             print(index)
 
-        baseline_dataset_df = pd.DataFrame(baseline_dataset)
-        ground_truth_df = pd.DataFrame(ground_truth)
+    baseline_dataset_df = pd.DataFrame(baseline_dataset)
+    ground_truth_df = pd.DataFrame(ground_truth)
 
-        return baseline_dataset_df, ground_truth_df
+    return baseline_dataset_df, ground_truth_df
 
 
 def highlight_sentences(sentence, annotations, entity_color_dict):
@@ -125,6 +125,8 @@ def highlight_dataset(baseline_dataset_df, ground_truth_df):
     color_list = (np.array(color_list) * 255).astype(int)
     entity_types = ground_truth_df['entity_type'].unique()
     entity_color_dict = {entity_type: color_list[index, :] for index, entity_type in enumerate(entity_types)}
+
+    ground_truth_df = ground_truth_df.sort_values(['sentence_id', 'start_char'])
 
     for entity_type, color in entity_color_dict.items():
         complete_text += f"<span style='color: rgb({color[0]}, {color[1]}, " \
