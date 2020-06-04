520a53e10ed508d750d9e9de5aab296b4ffd300c
Stanislav Schmidt <stanislav.schmidt@epfl.ch>
2020-06-04 16:26:16 +0200

    Finish SearchServer

--- src/bbsearch/server/search_server.py
+++ src/bbsearch/server/search_server.py
@@ -1,12 +1,30 @@
-import logging
-import textwrap
+from flask import request, jsonify
 
-from flask import request, jsonify, make_response
-
-logger = logging.getLogger(__name__)
+from ..local_searcher import LocalSearcher
 
 
-class EmbeddingServer:
+class SearchServer:
 
-    def __init__(self):
-        pass
+    def __init__(self,
+                 app,
+                 trained_models_path,
+                 embeddings_path,
+                 databases_path):
+        self.app = app
+        self.local_searcher = LocalSearcher(trained_models_path, embeddings_path, databases_path)
+
+        app.route("/", methods=["POST"])(self.query)
+
+    def query(self):
+        if request.is_json:
+            json_request = request.get_json()
+            sentence_ids, similarities, stats = self.local_searcher.query(**json_request)
+            result = dict(
+                sentence_ids=sentence_ids,
+                similarities=similarities,
+                stats=stats)
+        else:
+            result = {}
+
+        response = jsonify(result)
+        return response
