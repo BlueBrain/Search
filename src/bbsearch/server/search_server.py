cd8ec33a594c3d0cabafdcedaa9823ba2fe49ac2
Stanislav Schmidt <stanislav.schmidt@epfl.ch>
2020-06-04 10:55:58 +0200

    Add search server stub

---
+++ src/bbsearch/server/search_server.py
@@ -0,0 +1,12 @@
+import logging
+import textwrap
+
+from flask import request, jsonify, make_response
+
+logger = logging.getLogger(__name__)
+
+
+class EmbeddingServer:
+
+    def __init__(self):
+        pass
