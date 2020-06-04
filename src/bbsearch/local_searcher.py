199434478cb7932cf01b4ae970c57119dbec800d
Stanislav Schmidt <stanislav.schmidt@epfl.ch>
2020-06-04 13:49:27 +0200

    Add logging to LocalSearcher

--- src/bbsearch/local_searcher.py
+++ src/bbsearch/local_searcher.py
@@ -1,4 +1,5 @@
-import nltk
+import logging
+
 import numpy as np
 import sqlite3
 
@@ -7,19 +8,27 @@ from .search import run_search
 from .embedding_models import BSV, SBioBERT
 
 
+logger = logging.getLogger(__name__)
+
+
 class LocalSearcher(Searcher):
 
     def __init__(self, trained_models_path, embeddings_path, databases_path):
-
+        logger.info("Initializing embedding models...")
+        bsv_model_name = "BioSentVec_PubMed_MIMICIII-bigram_d700.bin"
+        bsv_model_path = trained_models_path / bsv_model_name
         self.embedding_models = {
-            "BSV":  BSV(checkpoint_model_path=trained_models_path / 'BioSentVec_PubMed_MIMICIII-bigram_d700.bin'),
+            "BSV":  BSV(checkpoint_model_path=bsv_model_path),
             "SBioBERT": SBioBERT()
         }
-        self.precomputed_embeddings = {
-            model_name: np.load(embeddings_path / f'{model_name}.npy').astype('float32')
-            for model_name in self.embedding_models}
-        # astype('float32') speeds up the search
 
+        logger.info("Loading precomputed embeddings...")
+        self.precomputed_embeddings = {
+            model_name: np.load(embeddings_path / f"{model_name}.npy").astype(np.float32)
+            for model_name in self.embedding_models}
+        # astype(np.float32) speeds up the search
+
+        logger.info("Connecting to the Cord19 database...")
         db_path = databases_path / "cord19.db"
         assert db_path.is_file()
         self.database_connection = sqlite3.connect(str(db_path))
