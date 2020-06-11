c24299e165b94af02cdc50ba4d20705365f7ee9c
Stanislav Schmidt <stanislav.schmidt@epfl.ch>
2020-06-11 11:13:13 +0200

    Add dummy CSV response to mining server

--- src/bbsearch/server/mining_server.py
+++ src/bbsearch/server/mining_server.py
@@ -1,6 +1,10 @@
+import csv
+import io
 import logging
 
-from flask import jsonify
+from flask import jsonify, make_response
+import pandas as pd
+
 
 logger = logging.getLogger(__name__)
 
@@ -24,5 +28,16 @@ class MiningServer:
         return jsonify(response)
 
     def pipeline(self):
-        csv = ""
-        return csv
+        df = pd.DataFrame([
+            {"entity_type": "DRUG", "entity": "paracetamol"},
+            {"entity_type": "ORGAN", "entity": "heart"}
+        ])
+
+        csv_file_buffer = io.StringIO()
+        df.to_csv(csv_file_buffer, index=False)
+
+        response = make_response(csv_file_buffer.getvalue())
+        response.headers["Content-Disposition"] = "attachment; filename=mining_results.csv"
+        response.headers["Content-Type"] = "text/csv"
+
+        return response
