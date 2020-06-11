01109e90e8d2d63bac8c72fc691f1057a958ca07
Stanislav Schmidt <stanislav.schmidt@epfl.ch>
2020-06-11 10:33:34 +0200

    Create minimal stub for the mining server.

---
+++ src/bbsearch/server/mining_server.py
@@ -0,0 +1,28 @@
+import logging
+
+from flask import jsonify
+
+logger = logging.getLogger(__name__)
+
+
+class MiningServer:
+
+    def __init__(self, app):
+        self.version = "1.0"
+        self.name = "MiningServer"
+
+        self.app = app
+        self.app.route("/", methods=["POST"])(self.pipeline)
+        self.app.route("/identify", methods=["POST"])(self.pipeline)
+
+    def identify(self):
+        response = {
+            "name": self.name,
+            "version": self.version,
+        }
+
+        return jsonify(response)
+
+    def pipeline(self):
+        csv = ""
+        return csv
