52c54996936b32a9509a6111b545a239b6da20cb
Jan Krepl <jankrepl@yahoo.com>
2020-06-19 18:23:11 +0200

    Start changing the server

--- src/bbsearch/server/mining_server.py
+++ src/bbsearch/server/mining_server.py
@@ -2,12 +2,13 @@
 import io
 import logging
 import pathlib
+import sqlite3
 
 from flask import jsonify, make_response, request
+import pandas as pd
 import spacy
 
-from ..mining import ChemProt, TextMiningPipeline
-
+from ..mining import run_pipeline
 
 logger = logging.getLogger(__name__)
 
@@ -21,29 +22,30 @@ class MiningServer:
         The Flask app wrapping the server.
     models_path : str or pathlib.Path
         The folder containing pre-trained models.
+    database_path : str or pathlib.Path
+        Path to the sql database.
     """
 
-    def __init__(self, app, models_path):
+    def __init__(self, app, models_path, database):
         self.version = "1.0"
         self.name = "MiningServer"
         self.models_path = pathlib.Path(models_path)
+        self.database_path = pathlib.Path(database_path)
 
         self.app = app
-        self.app.route("/", methods=["POST"])(self.pipeline)
+        self.app.route("/text", methods=["POST"])(self.pipeline_text)
+        self.app.route("/database", methods=["POST"])(self.pipeline_database)
         self.app.route("/help", methods=["POST"])(self.help)
 
         # Entities Extractors (EE)
-        ee_model = spacy.load("en_ner_craft_md")
+        self.ee_model = spacy.load("en_ner_craft_md")
 
         # Relations Extractors (RE)
-        chemprot_model_path = self.models_path / 'scibert_chemprot.tar.gz'
-        re_models = {('CHEBI', 'GGP'): [ChemProt(chemprot_model_path)]}
-
-        # Full Pipeline
-        self.text_mining_pipeline = TextMiningPipeline(ee_model, re_models)
+        self.re_models = {}
 
     def help(self):
         """Help the user by sending information about the server."""
+
         response = {
             "name": self.name,
             "version": self.version,
@@ -53,22 +55,29 @@ class MiningServer:
                     "description": "Get this help.",
                     "response_content_type": "application/json"
                 },
-                "/": {
-                    "description": "Compute text mining pipeline"
-                                   "(extract entities and relations).",
+                "/text": {
+                    "description": "Extract entities and relations from a given text.",
                     "response_content_type": "text/csv",
                     "required_fields": {
                         "text": []
                     },
                     "accepted_fields": {
-                        "article_id": [],
-                        "return_prob": [True, False],
+                        "debug": [True, False]
+                    }
+                },
+                "/database": {
+                    "description": "Extract entities and relations for given paragraph ids from "
+                                   "the database",
+                    "response_content_type": "text/csv",
+                    "required_fields": {
+                        "paragraph_ids": [],
+                    },
+                    "accepted_fields": {
                         "debug": [True, False]
                     }
                 }
             }
         }
-
         return jsonify(response)
 
     @staticmethod
@@ -89,23 +98,48 @@ class MiningServer:
 
         return response
 
-    def pipeline(self):
-        """Respond to a mining query."""
+    def pipeline_database(self):
+        """Respond to a query on specific paragraphs in the database."""
+        if request.is_json:
+            json_request = request.get_json()
+            paragraph_ids = json_request.get("paragraph_ids")
+            debug = json_request.get("debug", False)
+
+            if paragraph_ids is None:
+                response = self.make_error_response("The request text is missing.")
+            else:
+                paragraph_ids_joined = ','.join(f"\"{id_}\"" for id_ in set(paragraph_ids))
+
+                with sqlite3.connect(str(self.database_path)) as db_cnxn:
+                    sql_query = f"SELECT paragraph_id, section_name, text FROM paragraphs WHERE" \
+                                f" paragraph_id IN ({paragraph_ids_joined })"
+
+                    texts_df = pd.read_sql(sql_query, db_cnxn)
+                    texts = []  # TOD
+                df = run_pipeline(texts, self.ee_model, self.re_models, debug=debug)
+
+                with io.StringIO() as csv_file_buffer:
+                    df.to_csv(csv_file_buffer, index=False)
+                    response = make_response(csv_file_buffer.getvalue())
+                response.headers["Content-Type"] = "text/csv"
+                response.headers["Content-Disposition"] = "attachment; filename=bbs_mining_results.csv"
+
+        else:
+            response = self.make_error_response("The request has to be a JSON object.")
+
+        return response
+
+    def pipeline_text(self):
+        """Respond to a custom text query."""
         if request.is_json:
             json_request = request.get_json()
             text = json_request.get("text")
-            article_id = json_request.get("article_id")
-            return_prob = json_request.get("return_prob", False)
             debug = json_request.get("debug", False)
 
             if text is None:
                 response = self.make_error_response("The request text is missing.")
             else:
-                df = self.text_mining_pipeline(
-                    text=text,
-                    article_id=article_id,
-                    return_prob=return_prob,
-                    debug=debug)
+                df = run_pipeline([(text, {})], self.ee_model, self.re_models, debug=debug)
 
                 with io.StringIO() as csv_file_buffer:
                     df.to_csv(csv_file_buffer, index=False)
