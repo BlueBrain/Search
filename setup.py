b1b8379f67581e5777e2b3eaaf7f4254ee0aec7d
Emilie Delattre <emilie.delattre@epfl.ch>
2020-07-07 15:30:18 +0200

    Add sqlalchemy to setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,6 @@ description = "Blue Brain Search"
 install_requires = [
     'Flask',
     'ipywidgets',
-    'mysql-connector-python',
     'numpy>=1.16.1',
     'pandas',
     'pdfkit',
@@ -18,6 +17,7 @@ install_requires = [
     'sent2vec-prebuilt',
     'sentence-transformers',
     'scispacy',
+    'sqlalchemy',
     'en-core-web-sm @ https://github.com/explosion/spacy-models/releases/download/en_core_web_sm-2.2.5/en_core_web_sm-2.2.5.tar.gz',
     'en-ner-craft-md @ https://s3-us-west-2.amazonaws.com/ai2-s2-scispacy/releases/v0.2.4/en_ner_craft_md-0.2.4.tar.gz',
     'spacy==2.2.1',
