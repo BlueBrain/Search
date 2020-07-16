7c4b8c97753c193f54d7d0a2f427c13b04d9a64e
Stanislav Schmidt <stanislav.schmidt@epfl.ch>
2020-07-16 12:19:32 +0200

    Add a stub for the MiningWidget

--- src/bbsearch/widgets/__init__.py
+++ src/bbsearch/widgets/__init__.py
@@ -1,6 +1,8 @@
+from .mining_widget import MiningWidget
 from .search_widget import SearchWidget
 
 
 __all__ = [
+    'MiningWidget',
     'SearchWidget',
 ]
---
+++ src/bbsearch/widgets/mining_widget.py
@@ -0,0 +1,7 @@
+import ipywidgets as widgets
+
+
+class MiningWidget(widgets.VBox):
+
+    def __init__(self):
+        super().__init__()
