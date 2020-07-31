bbaa507e0fe43d1ed1584ec9b1a11ac7cf51fa64
Francesco Casalegno <francesco.casalegno@gmail.com>
2020-07-31 16:37:27 +0200

    Try to add .css in installation

--- setup.py
+++ setup.py
@@ -54,6 +54,8 @@ setup(
     },
     package_dir={"": "src"},
     packages=find_packages("./src"),
+    package_data={'': ['./src/bbsearch/_css/stylesheet.css']},
+    include_package_data=True,
     python_requires=">=3.6",
     setup_requires=["setuptools_scm"],
     install_requires=install_requires,
