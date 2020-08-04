def41ba2bc46f33d40e35de8426969a5397749b3
Stanislav Schmidt <stanislav.schmidt@epfl.ch>
2020-08-04 14:27:36 +0100

    Update resource inclusion

--- src/bbsearch/_css/style.py
+++ src/bbsearch/_css/style.py
@@ -1,11 +1,10 @@
 """CSS styling utilities."""
-
-import pathlib
+import pkg_resources
 
 
 def get_css_style():
     """Get content of CSS style sheet."""
-    css_file = pathlib.Path(__file__).parents[0] / 'stylesheet.css'
+    css_file = pkg_resources.resource_filename(__name__, "stylesheet.css")
     with open(css_file, 'r') as f:
         css_style = f.read()
     return css_style
