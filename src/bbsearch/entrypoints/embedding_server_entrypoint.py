138ead0de97f390f090609f55acd620bfa6cb99a
Francesco Casalegno <francesco.casalegno@gmail.com>
2020-08-05 15:13:21 +0200

    Replace /raid/bbs_data with /raid/sync/proj115/bbs_data (#85)
    Co-authored-by: Stanislav Schmidt <stanislav.schmidt@epfl.ch>

--- src/bbsearch/entrypoints/embedding_server_entrypoint.py
+++ src/bbsearch/entrypoints/embedding_server_entrypoint.py
@@ -15,7 +15,7 @@ parser.add_argument("--port",
                     type=int,
                     help="The server port")
 parser.add_argument("--bsv_checkpoints",
-                    default='/raid/covid_data/assets/BioSentVec_PubMed_MIMICIII-bigram_d700.bin',
+                    default='/raid/sync/proj115/bbs_data/trained_models/BioSentVec_PubMed_MIMICIII-bigram_d700.bin',
                     type=str,
                     help="Path to file containing the checkpoints for the BSV model.")
 args = parser.parse_args()
