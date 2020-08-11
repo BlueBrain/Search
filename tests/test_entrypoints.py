95e8d39b983c7b79987cd402c694549695853c26
Stanislav Schmidt <stanislav.schmidt@epfl.ch>
2020-08-11 17:22:31 +0100

    Some experimental black formatting

--- tests/test_entrypoints.py
+++ tests/test_entrypoints.py
@@ -5,7 +5,13 @@ import pytest
 
 @pytest.mark.parametrize(
     "entrypoint_name",
-    ["embedding_server", "create_database", "compute_embeddings", "search_server", "mining_server"]
+    [
+        "embedding_server",
+        "create_database",
+        "compute_embeddings",
+        "search_server",
+        "mining_server",
+    ],
 )
 def test_entrypoint(entrypoint_name):
     subprocess.check_call([entrypoint_name, "--help"])
--- tests/test_remote_searcher.py
+++ tests/test_remote_searcher.py
@@ -6,26 +6,28 @@ from requests.models import Response
 from bbsearch.remote_searcher import RemoteSearcher
 
 
-@pytest.mark.parametrize('status', [True, False])
+@pytest.mark.parametrize("status", [True, False])
 def test_all(monkeypatch, status):
     fake_response = Mock(spec=Response)
-    fake_response.json.return_value = {'sentence_ids': 'A',
-                                       'similarities': 'B',
-                                       'stats': 'C'}
+    fake_response.json.return_value = {
+        "sentence_ids": "A",
+        "similarities": "B",
+        "stats": "C",
+    }
 
     fake_response.ok = status
 
     fake_requests = Mock()
     fake_requests.post.return_value = fake_response
 
-    monkeypatch.setattr('bbsearch.remote_searcher.requests', fake_requests)
-    searcher = RemoteSearcher('')
-    sentence_ids, similarities, stats = searcher.query('model', 2, 'some_text')
+    monkeypatch.setattr("bbsearch.remote_searcher.requests", fake_requests)
+    searcher = RemoteSearcher("")
+    sentence_ids, similarities, stats = searcher.query("model", 2, "some_text")
 
     if status:
-        assert sentence_ids == 'A'
-        assert similarities == 'B'
-        assert stats == 'C'
+        assert sentence_ids == "A"
+        assert similarities == "B"
+        assert stats == "C"
     else:
         assert sentence_ids is None
         assert similarities is None
