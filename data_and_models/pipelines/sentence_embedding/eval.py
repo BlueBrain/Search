2d1dc36aca03fbc2af5033a2f8d7920310ddf5df
Francesco Casalegno <francesco.casalegno@gmail.com>
2020-10-16 13:45:59 +0200

    More outputs in eval

--- data_and_models/pipelines/sentence_embedding/eval.py
+++ data_and_models/pipelines/sentence_embedding/eval.py
@@ -5,6 +5,7 @@ import pathlib
 from argparse import ArgumentParser
 from collections import OrderedDict
 
+import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 import yaml
@@ -21,14 +22,18 @@ parser.add_argument(
     "be comma-separated.",
 )
 parser.add_argument(
-    "--model", required=True, type=str, help="Name of the model to evaluate.",
-)
-parser.add_argument(
-    "--output_file",
+    "--model",
     required=True,
     type=str,
-    help="Output json file where metrics results should be written.",
+    help="Name of the model to evaluate.",
 )
+parser.add_argument(
+    "--output_dir",
+    required=True,
+    type=str,
+    help="Output dir where metrics and outputs  will be written.",
+)
+
 args = parser.parse_args()
 
 
@@ -43,6 +48,7 @@ def main():
     df_sents = pd.read_csv(args.annotation_files)
 
     print("Computing test scores...")
+    print(" - test 1: correlation metrics")
     y_true = df_sents["score"]
     embeddings_1 = model.embed_many(model.preprocess_many(df_sents["sentence_1"]))
     embeddings_2 = model.embed_many(model.preprocess_many(df_sents["sentence_2"]))
@@ -52,16 +58,57 @@ def main():
             for e1, e2 in zip(embeddings_1, embeddings_2)
         ]
     )
-
     metrics_dict = OrderedDict()
     metrics_dict["kendall_tau"] = kendalltau(y_true, y_pred).correlation
     metrics_dict["pearson_r"] = pearsonr(y_true, y_pred)[0]
     metrics_dict["spearman_rho"] = spearmanr(y_true, y_pred).correlation
 
-    print("Writing to file...")
-    output_file = pathlib.Path(args.output_file)
-    with output_file.open("w") as f:
+    print(" - test 2: semantic similarities matrix")
+    # Matrix inspired by Cer D., et al. "Universal sentence encoder." arXiv
+    # preprint arXiv:1803.11175 (2018).
+    words_of_interest = [
+        "COVID-19",
+        "Sars-Cov-2",
+        "coronavirus",
+        "sugar",
+        "glucose",
+        "obesity",
+        "glycosylation",
+        "ketosis",
+        "potato",
+        "carrot",
+    ]
+    embeddings_w = model.embed_many(model.preprocess_many(words_of_interest))
+    mm = cosine_similarity(embeddings_w, embeddings_w)
+
+    print("Writing results to file...")
+    out_file = pathlib.Path(args.output_dir) / args.model
+    # Write metrics
+    output_json = out_file.with_suffix(".json")
+    with output_json.open("w") as f:
         json.dump(metrics_dict, f)
+
+    # Write matrix
+    output_csv = out_file.with_suffix(".csv")
+    output_png = out_file.with_suffix(".png")
+    pd.DataFrame(mm, columns=words_of_interest, index=words_of_interest).to_csv(
+        output_csv
+    )
+    # Plot matrix
+    f, ax = plt.subplots(figsize=(5,) * 2)
+    ax.imshow(mm)
+    nw = len(embeddings_w)
+    for i in range(nw):
+        for j in range(nw):
+            c = "white" if mm[i, j] < 0.5 else "black"
+            ax.text(i, j, f"{mm[i, j]:.1f}", ha="center", va="center", c=c)
+    ax.set_xticks(np.arange(nw))
+    ax.set_yticks(np.arange(nw))
+    ax.set_xticklabels(words_of_interest, rotation=45)
+    ax.set_yticklabels(words_of_interest)
+    ax.xaxis.tick_top()
+    f.tight_layout()
+    f.savefig(output_png, transparent=True, dpi=200)
 
     print("Done.")
 
