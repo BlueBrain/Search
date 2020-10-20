e72e1f5cea2d1668e850ab8bb696176a2dd845dd
Jan <jankrepl@yahoo.com>
2020-10-20 11:47:30 +0200

    [BBS-171] Create framework for patterns creation (#147)

--- src/bbsearch/mining/entity.py
+++ src/bbsearch/mining/entity.py
@@ -2,7 +2,338 @@
 
 import copy
 
+import numpy as np
+import pandas as pd
 import spacy
+
+from bbsearch.utils import JSONL
+
+
+class PatternCreator:
+    """Utility class for easy handling of patterns.
+
+    Parameters
+    ----------
+    storage : None or pd.DataFrame
+        If provided, we automatically populate `_storage` with it. If None, then
+        we start from scratch - no patterns.
+
+    Attributes
+    ----------
+    _storage : pd.DataFrame
+        A representation of all patterns allows for comfortable sorting,
+        filtering, etc. Note that each row represents a single pattern.
+
+    Examples
+    --------
+    >>> from bbsearch.mining import PatternCreator
+    >>>
+    >>> pc = PatternCreator()
+    >>> pc.add("FOOD", [{"LOWER": "oreo"}])
+    >>> pc.add("DRINK", [{"LOWER": {"REGEX": "^w"}}, {"LOWER": "milk"}])
+    >>> doc = pc("It is necessary to dip the oreo in warm milk!")
+    >>> [(str(e), e.label_) for e in doc.ents]
+    [('oreo', 'FOOD'), ('warm milk', 'DRINK')]
+    """
+
+    def __init__(self, storage=None):
+        if storage is None:
+            columns = ["label"]
+            self._storage = pd.DataFrame(columns=columns)
+        else:
+            self._storage = storage.reset_index(drop=True)
+
+    def __call__(self, text, model=None, disable=None, **add_pipe_kwargs):
+        """Test the current patterns on text.
+
+        Parameters
+        ----------
+        text : str
+            Some text.
+
+        model : spacy.language.Language or None
+            Spacy model. If not provided we default to `spacy.blank("en")`.
+
+        disable : list or None
+            List of elements to remove from the pipeline.
+
+        **add_pipe_kwargs : dict
+            Additionally parameters to be passed into the `add_pipe` method. Note that
+            one can control the position the ``EntityRuler`` this way. If not specified
+            we put it at the very end.
+
+        Returns
+        -------
+        doc : spacy.Doc
+            Doc containing the entities under the `ents` property.
+        """
+        model = model or spacy.blank("en")
+        disable = disable or []
+        add_pipe_kwargs = add_pipe_kwargs or {"last": True}
+        er = spacy.pipeline.EntityRuler(model, patterns=self.to_list(), validate=True)
+        model.add_pipe(er, **add_pipe_kwargs)
+
+        return model(text, disable=disable)
+
+    def __eq__(self, other):
+        """Determine if equal.
+
+        Parameters
+        ----------
+        other : PatternCreator
+            Some other PatternCreator that we wish to compare to.
+
+        Returns
+        -------
+        bool
+            If True, the patterns are identical. Note that the order does not matter.
+        """
+        if not isinstance(other, self.__class__):
+            return False
+
+        self_df_unsorted = self.to_df()
+        other_df_unsorted = other.to_df()
+
+        if set(self_df_unsorted.columns) != set(other_df_unsorted.columns):
+            return False
+
+        sort_by = list(self_df_unsorted.columns)
+        self_df_sorted = self_df_unsorted.sort_values(by=sort_by)
+        other_df_sorted = other_df_unsorted.sort_values(by=sort_by)
+
+        self_is_nan = self_df_sorted.isnull().values
+        other_is_nan = other_df_sorted.isnull().values
+
+        return np.array_equal(self_is_nan,
+                              other_is_nan) and np.array_equal(self_df_sorted.values[~self_is_nan],
+                                                               other_df_sorted.values[~other_is_nan])
+
+    def add(self, label, pattern, check_exists=True):
+        """Add a single raw in the patterns.
+
+        Parameters
+        ----------
+        label : str
+            Entity type to associate with a given pattern.
+
+        pattern : str or dict or list
+            The pattern we want to match. The behavior depends on the type.
+
+            - ``str``: can be used for exact matching (case sensitive). We internally convert
+              it to a single-token pattern `{"TEXT": pattern}`.
+            - ``dict``: a single-token pattern. This dictionary can contain at most 2 entries.
+              The first one represents the attribute: value pair ("LEMMA": "world"). The second
+              has a key "OP" and is optional. It represents the operator/quantifier to be used.
+              An example of a valid pattern dict is `{"LEMMA": "world", "OP": "+"}`. Note that
+              it would detect entities like "world" and "world world world".
+            - ``list``: a multi-token pattern. A list of dictionaries that are of the same form
+              as described above.
+
+        check_exists : bool
+            If True, we only allow to add patterns that do not exist yet.
+        """
+        if isinstance(pattern, str):
+            pattern_ = [{"TEXT": pattern}]
+
+        elif isinstance(pattern, dict):
+            pattern_ = [pattern]
+
+        elif isinstance(pattern, list):
+            pattern_ = pattern
+
+        else:
+            raise TypeError("Unsupported type of pattern")
+
+        new_row = self.raw2row({"label": label, "pattern": pattern_})
+
+        new_storage = self._storage.append(new_row.to_frame().T, ignore_index=True)
+        if check_exists and new_storage.duplicated().any():
+            raise ValueError("The pattern already exists")
+
+        self._storage = new_storage
+
+    def drop(self, labels):
+        """Drop one or multiple patterns.
+
+        Parameters
+        ----------
+        labels : int or list
+            If ``int`` then represent a row index to be dropped. If ``list`` then
+            a collection of row indices to be dropped.
+        """
+        self._storage = self._storage.drop(index=labels).reset_index(drop=True)
+
+    def to_df(self):
+        """Convert to a pd.DataFrame.
+
+        Returns
+        -------
+        pd.DataFrame
+            Copy of the `_storage`. Each row represents a single entity type pattern. All
+            elements are strings.
+        """
+        return self._storage.copy()
+
+    def to_list(self, sort_by=None):
+        """Convert to a list.
+
+        Parameters
+        ----------
+        sort_by : None or list
+            If None, then no sorting taking place. If ``list``, then the names of columns
+            along which to sort.
+
+        Returns
+        -------
+        list
+            A list where each element represents one entity type pattern. Note that
+            this list can be directly passed into the `EntityRuler`.
+        """
+        storage = self.to_df()
+        sorted_storage = storage.sort_values(by=sort_by) if sort_by is not None else storage
+        return [self.row2raw(row) for _, row in sorted_storage.iterrows()]
+
+    def to_jsonl(self, path, sort_by=None):
+        """Save to jsonl.
+
+        Parameters
+        ----------
+        sort_by : None or list
+            If None, then no sorting taking place. If ``list``, then the names of columns
+            along which to sort.
+        """
+        patterns = self.to_list(sort_by=sort_by)
+        JSONL.dump_jsonl(patterns, path)
+
+    @classmethod
+    def from_jsonl(cls, path):
+        """Load from a jsonl file.
+
+        Parameters
+        ----------
+        path : pathlib.Path
+            Path to a jsonl file with patterns.
+
+        Returns
+        -------
+        pattern_creator : bbsearch.mining.PatternCreator
+            Instance of a ``PatternCreator``.
+        """
+        inst = cls()
+        patterns = JSONL.load_jsonl(path)
+
+        for p in patterns:
+            inst.add(label=p["label"], pattern=p["pattern"])
+
+        return inst
+
+    @staticmethod
+    def raw2row(raw):
+        """Convert an element of patterns list to a pd.Series.
+
+        The goal of this function is to create a pd.Series
+        with all entries being strings. This will allow us
+        to check for duplicates between different rows really
+        quickly.
+
+        Parameters
+        ----------
+        raw : dict
+            Dictionary with two keys: "label" and "pattern".
+            The `pattern` needs to be a list of dictionaries
+            each representing a pattern for a given token.
+            The `label` is a string representing the entity type.
+
+        Returns
+        -------
+        row : pd.Series
+            The index contains the following elements: "label",
+            "attribute_0", "value_0", "value_type_0", "op_0",
+            "attribute_1", "value_1", "value_type_1", "op_1",
+            ...
+
+        """
+        if not isinstance(raw["label"], str):
+            raise TypeError("The label needs to be a string")
+
+        if not isinstance(raw["pattern"], list):
+            raise TypeError("The pattern needs to be a list")
+
+        d = {"label": raw["label"]}
+        for token_ix, e in enumerate(raw["pattern"]):
+            if not isinstance(e, dict):
+                raise TypeError("The per token pattern needs to be a dictionary")
+
+            if len(e) == 1:
+                pass
+            elif len(e) == 2 and "OP" in e:
+                pass
+            else:
+                raise ValueError("Invalid element, multi-attribute matches are not supported")
+
+            attribute = next(filter(lambda key: key != "OP", e))
+            value_type = type(e[attribute]).__name__
+            value = str(e[attribute])
+            op = e.get("OP", "")
+
+            d.update({f"attribute_{token_ix}": attribute,
+                      f"value_{token_ix}": value,
+                      f"value_type_{token_ix}": value_type,
+                      f"op_{token_ix}": op})
+        return pd.Series(d)
+
+    @staticmethod
+    def row2raw(row):
+        """Convert pd.Series to a valid pattern dictionary.
+
+        Note that the `value_{i}` is always a string, however,
+        we cast it to `value_type_{i}` type. In most cases the
+        type will be ``int``, ``str`` or ``dict``. Since
+        this casting is done dynamically we use `eval`.
+
+        Parameters
+        ----------
+        row : pd.Series
+            The index contains the following elements: "label",
+            "attribute_0", "value_0", "value_type_0", "op_0",
+            "attribute_1", "value_1", "value_type_1", "op_1",
+
+        Returns
+        -------
+        raw : dict
+            Dictionary with two keys: "label" and "pattern".
+            The `pattern` needs to be a list of dictionaries
+            each representing a pattern for a given token.
+            The `label` is a string representing the entity type.
+        """
+        pattern = []
+        token_ix = 0
+        while True:
+            try:
+                attribute = row[f"attribute_{token_ix}"]  # str
+                value_str = row[f"value_{token_ix}"]  # str
+                value_type = row[f"value_type_{token_ix}"]  # str
+                op = row[f"op_{token_ix}"]  # str
+
+                if any(not isinstance(x, str) for x in [attribute, value_str, value_type, op]):
+                    raise KeyError()
+
+                value = eval(f"{value_type}({value_str})") if value_type != 'str' else value_str
+
+                token_pattern = {attribute: value}
+                if op:
+                    token_pattern["OP"] = op
+
+                pattern.append(token_pattern)
+            except KeyError:
+                break
+
+            token_ix += 1
+
+        if token_ix == 0:
+            raise ValueError("No valid pattern was found")
+
+        return {"label": row["label"], "pattern": pattern}
 
 
 def remap_entity_type(patterns, etype_mapping):
--- tests/test_mining/test_entity.py
+++ tests/test_mining/test_entity.py
@@ -1,13 +1,213 @@
 """Collections of tests covering the `entity.py` module."""
+import pathlib
+
+import numpy as np
 import pandas as pd
 import pytest
 import spacy
 
 from bbsearch.mining import (
+    PatternCreator,
     check_patterns_agree,
     global2model_patterns,
     remap_entity_type,
 )
+
+
+class TestPatternCreator:
+    def test_equality(self):
+        pc1 = PatternCreator()
+        pc2 = PatternCreator()
+        pc2.add("ETYPE", "hello")
+
+        assert pc1 == pc1
+        assert pc1 != "wrong type"
+        assert pc1 != pc2  # different columns
+
+    def test_drop(self):
+        pc = PatternCreator()
+
+        pc.add("ET1", "hello")
+        pc.add("ET1", "there")
+        pc.add("ET2", "world")
+        pc.add("ET4", "dog")
+
+        assert pc.to_df().index.to_list() == [0, 1, 2, 3]
+
+        pc.drop([1, 2])
+
+        assert pc.to_df().index.to_list() == [0, 1]
+
+    def test_to_df(self):
+        pc = PatternCreator()
+
+        pc.add("ET1", "hello")
+        pc.add("ET1", "there")
+
+        df_1 = pc.to_df()
+        df_2 = pc.to_df()
+
+        df_2.loc[0, "label"] = "REPLACED_LABEL"
+
+        df_3 = pc.to_df()
+
+        assert not df_1.equals(df_2)
+        assert df_1.equals(df_3)
+
+    def test_to_list(self):
+        pc = PatternCreator()
+
+        pc.add("ET1", "hello")
+        pc.add("ET2", {"TEXT": "there"})
+        pc.add("ET3", [{"TEXT": {"IN": ["world", "cake"]}}])
+        pc.add("ET4", [{"TEXT": {"IN": ["aa", "bbb"]}},
+                       {"TEXT": {"REGEX": "^s"}}])
+
+        res = pc.to_list()
+
+        assert len(res) == 4
+
+    def test_errors(self):
+        pc = PatternCreator()
+
+        with pytest.raises(TypeError):
+            pc.add("ETYPE", 234324)  # invalid type
+
+        pc.add("ETYPE", "hello")
+
+        with pytest.raises(ValueError):
+            pc.add("ETYPE", "hello", check_exists=True)  # duplicate
+
+        with pytest.raises(ValueError):
+            pc.add("etype", [{"a": 1, "b": 2}])  # wrong contents
+
+    def test_overall(self, tmpdir):
+        tmpdir_p = pathlib.Path(str(tmpdir)) / "patterns.json"
+
+        pc = PatternCreator()
+
+        assert len(pc.to_df()) == 0
+
+        pc.add("NEW_ENTITY_TYPE", "cake")
+
+        assert len(pc.to_df()) == 1
+        assert set(pc.to_df().columns) == {"label",
+                                           "attribute_0",
+                                           "value_0",
+                                           "value_type_0",
+                                           "op_0"}
+
+        pc.add("COOL_ENTITY_TYPE", {"LEMMA": "pancake", "OP": "*"})
+
+        assert len(pc.to_df()) == 2
+
+        pc.add("SOME_ENTITY_TYPE", [{"TEXT": "good"}, {"TEXT": "pizza"}])
+
+        assert len(pc.to_df()) == 3
+        assert set(pc.to_df().columns) == {"label",
+                                           "attribute_0",
+                                           "value_0",
+                                           "value_type_0",
+                                           "op_0",
+                                           "attribute_1",
+                                           "value_1",
+                                           "value_type_1",
+                                           "op_1",
+                                           }
+
+        pc.to_jsonl(tmpdir_p)
+        pc_loaded = PatternCreator.from_jsonl(tmpdir_p)
+        pc_manual = PatternCreator(storage=pc.to_df())
+
+        assert pc == pc_loaded == pc_manual
+
+    def test_call(self):
+        pc = PatternCreator()
+
+        pc.add("new_entity_type", "tall")
+
+        text = "I saw a tall building."
+        doc = pc(text)
+        assert len(doc.ents) == 1
+        assert list(doc.ents)[0].label_ == "new_entity_type"
+
+        pc.drop(0)
+
+        doc2 = pc(text)
+
+        assert len(doc2.ents) == 0
+
+    def test_raw2row(self):
+        # pattern not a list
+        with pytest.raises(TypeError):
+            PatternCreator.raw2row({"label": "ET1",
+                                    "pattern": {"LOWER": "TEXT"}})
+
+        # label not a str
+        with pytest.raises(TypeError):
+            PatternCreator.raw2row({"label": 232,
+                                    "pattern": [{"LOWER": "TEXT"}]})
+
+        # element not dictionary
+        with pytest.raises(TypeError):
+            PatternCreator.raw2row({"label": "etype",
+                                    "pattern": [11]})
+
+    def test_row2raw(self):
+        # unsupported value_type - eval fails
+        with pytest.raises(NameError):
+            PatternCreator.row2raw(pd.Series({"label": "et1",
+                                              "attribute_0": "TEXT",
+                                              "value_0": "aaa",
+                                              "value_type_0": "wrong_type",
+                                              "op_0": ""}))
+
+        # already the first token is invalid
+        with pytest.raises(ValueError):
+            PatternCreator.row2raw(pd.Series({"label": "et1",
+                                              "attribute_0": np.nan,
+                                              "value_0": "aaa",
+                                              "value_type_0": "wrong_type",
+                                              "op_0": ""}))
+
+        res = PatternCreator.row2raw(pd.Series(
+            {"label": "et1",
+             "attribute_0": "TEXT",
+             "value_0": "aaa",
+             "value_type_0": "str",
+             "op_0": "",
+             "attribute_1": np.nan,
+             "value_1": "bbb",
+             "value_type_1": "int",
+             "op_1": "!"}
+        ))
+
+        assert res == {"label": "et1", "pattern": [{"TEXT": "aaa"}]}
+
+    @pytest.mark.parametrize("raw", [
+        {"label": "ET1",
+         "pattern": [{"LOWER": "something"}]},
+        {"label": "ET2",
+         "pattern": [{"REGEX": "^S"}]},
+        {"label": "ET3",
+         "pattern": [{"LEMMA": "bb", "OP": "!"}]},
+        {"label": "ET4",
+         "pattern": [{"OP": "+", "LOWER": "fdsaf"}]},
+        {"label": "ET5",
+         "pattern": [{"ORTH": "fdsaf"}, {"LEMMA": "aaa"}]},
+        {"label": "ET6",
+         "pattern": [{"OP": "+", "LOWER": "aaa"},
+                     {"OP": "!", "LEMMA": "bb"},
+                     {"OP": "?", "ORTH": "cc"}]},
+        {"label": "ET7",
+         "pattern": [{"LENGTH": 5}]},
+        {"label": "ET8",
+         "pattern": [{"TEXT": {"IN": ["aa", "bbb"]}},
+                     {"TEXT": {"REGEX": "^s"}}]}
+
+    ])
+    def test_raw2row2raw(self, raw):
+        assert raw == PatternCreator.row2raw(PatternCreator.raw2row(raw))
 
 
 def test_entity_type():
