3811b99090e572eff84e1730e550f3396b79eecb
Francesco Casalegno <francesco.casalegno@gmail.com>
2020-10-27 15:29:11 +0100

    Fix small bug 🐛 + Run black 🍰

--- data_and_models/pipelines/ner/interrater.py
+++ data_and_models/pipelines/ner/interrater.py
@@ -41,31 +41,36 @@ def main():
     df_a1 = annotations2df(args.annotations1.split(","))
     df_a2 = annotations2df(args.annotations2.split(","))
     # Merge the common sentences between annotators
-    df = df_a2.merge(df_a1,
-                     on=['source', 'id', 'text', 'start_char', 'end_char'],
-                     suffixes=('_annotator_2', '_annotator_1'),
-                     how='inner')
+    df = df_a2.merge(
+        df_a1,
+        on=["source", "id", "text", "start_char", "end_char"],
+        suffixes=("_annotator_2", "_annotator_1"),
+        how="inner",
+    )
 
     df = remove_punctuation(df)
 
     iob_true = df["class_annotator_1"]
     iob_pred = df["class_annotator_2"]
 
+    metrics_dict = {}
     for mode in ["entity", "token"]:
-        metrics_dict = ner_report(
+        metrics_dict[mode] = ner_report(
             iob_true,
             iob_pred,
             mode=mode,
             return_dict=True,
         )
-        for etype, metrics in metrics_dict.items():
-            etype_dict = OrderedDict(
-                [(f"{mode}_{k}", v) for k, v in metrics.items()]
+    for etype in metrics_dict["entity"]:
+        all_metrics_dict = OrderedDict()
+        for mode in ["entity", "token"]:
+            all_metrics_dict.update(
+                [(f"{mode}_{k}", v) for k, v in metrics_dict[mode][etype].items()]
             )
-            filename = etype.lower() + '.json'
-            output_file = pathlib.Path(args.output_dir) / filename
-            with output_file.open("w") as f:
-                json.dump(etype_dict, f)
+        filename = etype.lower() + ".json"
+        output_file = pathlib.Path(args.output_dir) / filename
+        with output_file.open("w") as f:
+            json.dump(all_metrics_dict, f)
 
 
 if __name__ == "__main__":
