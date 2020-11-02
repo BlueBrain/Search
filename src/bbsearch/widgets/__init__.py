f238c306ddbd1a6e35ac58314e89017478dea022
Stanislav Schmidt <stanislav.schmidt@epfl.ch>
2020-11-02 11:28:06 +0100

    Move MiningSchema to a separate module

--- src/bbsearch/widgets/__init__.py
+++ src/bbsearch/widgets/__init__.py
@@ -1,6 +1,7 @@
 """Various widgets related to the BBS."""
 from .article_saver import ArticleSaver
-from .mining_widget import MiningSchema, MiningWidget
+from .mining_schema import MiningSchema
+from .mining_widget import MiningWidget
 from .search_widget import SearchWidget
 
 __all__ = [
