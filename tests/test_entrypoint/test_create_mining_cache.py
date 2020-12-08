0a438f3e9214d0ba2cc28a4cb8226dc75147dd0d
Stanislav Schmidt <stanislav.schmidt@epfl.ch>
2020-12-08 13:38:38 +0100

    Add tests for mining cache entrypoint

--- tests/test_entrypoint/test_create_mining_cache.py
+++ tests/test_entrypoint/test_create_mining_cache.py
@@ -1,5 +1,6 @@
-import pytest
 from unittest.mock import Mock
+
+import pytest
 
 from bbsearch.entrypoint import run_create_mining_cache
 
@@ -12,6 +13,18 @@ def test_help(capsys):
     assert error.value.code == 0
     assert stdout.startswith("usage:")
     assert stderr == ""
+
+
+def test_invalid_db_type():
+    with pytest.raises(ValueError, match="^Invalid database type"):
+        run_create_mining_cache(["--db-type", "wrong-type"])
+
+
+def test_missing_sqlite_db():
+    with pytest.raises(FileNotFoundError, match="^No database found"):
+        run_create_mining_cache(
+            ["--db-type", "sqlite", "--database-url", "fake$?#"],
+        )
 
 
 @pytest.mark.parametrize(
@@ -27,8 +40,15 @@ def test_send_through(monkeypatch, db_type, database_url):
     fake_sqlalchemy = Mock()
     fake_create_mining_cache = Mock()
     monkeypatch.setattr("bbsearch.entrypoint.create_mining_cache.pd", fake_pandas)
-    monkeypatch.setattr("bbsearch.entrypoint.create_mining_cache.sqlalchemy", fake_sqlalchemy)
-    monkeypatch.setattr("bbsearch.database.mining_cache.CreateMiningCache", fake_create_mining_cache)
+    monkeypatch.setattr(
+        "bbsearch.entrypoint.create_mining_cache.sqlalchemy", fake_sqlalchemy
+    )
+    monkeypatch.setattr(
+        "bbsearch.database.mining_cache.CreateMiningCache", fake_create_mining_cache
+    )
+
+    # Create temporary sqlite database
+    return
 
     # Construct arguments
     argv = [
@@ -37,4 +57,4 @@ def test_send_through(monkeypatch, db_type, database_url):
     ]
 
     # Call entrypoint method
-    # run_create_mining_cache(argv)
+    run_create_mining_cache(argv)
