4bbf0841a79afa048e1266df04ef2ca5f4b9404d
Francesco Casalegno <francesco.casalegno@gmail.com>
2020-12-10 15:48:52 +0100

    Fix bug in bash

--- docker/mining.sh
+++ docker/mining.sh
@@ -12,7 +12,7 @@ printf "Host *\n    User %s" "$BBS_SSH_USERNAME"> ~/.ssh/config
 dvc remote modify gpfs_ssh ask_password true
 cd /src/data_and_models/pipelines/ner/ || exit
 dvc pull ee_models_library.csv.dvc
-dvc pull "$(< dvc.yaml grep -oE '\badd_er_[0-9]+\b' | xargs)"
+dvc pull $(< dvc.yaml grep -oE '\badd_er_[0-9]+\b' | xargs)
 
 # Launch mining server
 gunicorn --bind 0.0.0.0:8080 --workers 1 --timeout 7200 'bbsearch.entrypoints:get_mining_app()'
