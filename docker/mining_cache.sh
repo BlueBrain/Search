9db38ca709385d47f0f4c6abd01facb26d746147
Francesco Casalegno <francesco.casalegno@gmail.com>
2020-12-10 18:04:27 +0100

    Fix verbosity param

--- docker/mining_cache.sh
+++ docker/mining_cache.sh
@@ -19,4 +19,4 @@ create_mining_cache \
   $([ -n "$BBS_MINING_CACHE_TARGET_TABLE" ] && echo "--target-table-name $BBS_MINING_CACHE_TARGET_TABLE") \
   $([ -n "$BBS_MINING_CACHE_PROCESSORS_PER_MODEL" ] && echo "--n_processes_per_model $BBS_MINING_CACHE_PROCESSORS_PER_MODEL") \
   $([ -n "$BBS_MINING_CACHE_LOG_FILE" ] && echo "--log-file $BBS_MINING_CACHE_LOG_FILE") \
-  $([ -n "$BBS_MINING_VERBOSE" ] && echo "--verbose $BBS_MINING_VERBOSE")
+  $([ -n "$BBS_MINING_VERBOSE" ] && echo "$BBS_MINING_VERBOSE")
