c8c5080513a16fc44ef7c95919397f7b3d80702c
Francesco Casalegno <francesco.casalegno@gmail.com>
2020-12-15 10:47:51 +0100

    Integrate Stanislav feedback

--- docker/mining.sh
+++ docker/mining.sh
@@ -1,18 +1,9 @@
 #!/bin/bash
 
-# Ensure that the correct username is used for ssh
-if [[ -z "$BBS_SSH_USERNAME" ]]; then
-  echo "Env var BBS_SSH_USERNAME unset!" 1>&2
-  exit 1
-fi
-mkdir ~/.ssh
-printf "Host *\n    User %s" "$BBS_SSH_USERNAME"> ~/.ssh/config
-
-# Pull models with DVC
-dvc remote modify gpfs_ssh ask_password true
-cd /src/data_and_models/pipelines/ner/ || exit
-dvc pull ee_models_library.csv.dvc
-dvc pull $(< dvc.yaml grep -oE '\badd_er_[0-9]+\b' | xargs)
+# shellcheck source=.
+source "$($0)"/utils.sh
+ssh_setup
+dvc_pull_models
 
 # Launch mining server
 gunicorn --bind 0.0.0.0:8080 --workers 1 --timeout 7200 'bbsearch.entrypoints:get_mining_app()'
--- docker/mining_cache.sh
+++ docker/mining_cache.sh
@@ -1,22 +1,13 @@
 #!/bin/bash
 
-# Ensure that the correct username is used for ssh
-if [[ -z "$BBS_SSH_USERNAME" ]]; then
-  echo "Env var BBS_SSH_USERNAME unset!" 1>&2
-  exit 1
-fi
-mkdir ~/.ssh
-printf "Host *\n    User %s" "$BBS_SSH_USERNAME"> ~/.ssh/config
-
-# Pull models with DVC
-dvc remote modify gpfs_ssh ask_password true
-cd /src/data_and_models/pipelines/ner/ || exit
-dvc pull ee_models_library.csv.dvc
-dvc pull $(< dvc.yaml grep -oE '\badd_er_[0-9]+\b' | xargs)
+# shellcheck source=.
+source "$($0)"/utils.sh
+ssh_setup
+dvc_pull_models
 
 # Launch mining cache creation, using arguments only if defined
 create_mining_cache \
   $([ -n "$BBS_MINING_CACHE_TARGET_TABLE" ] && echo "--target-table-name $BBS_MINING_CACHE_TARGET_TABLE") \
-  $([ -n "$BBS_MINING_CACHE_PROCESSORS_PER_MODEL" ] && echo "--n_processes_per_model $BBS_MINING_CACHE_PROCESSORS_PER_MODEL") \
+  $([ -n "$BBS_MINING_CACHE_PROCESSORS_PER_MODEL" ] && echo "--n-processes-per-model $BBS_MINING_CACHE_PROCESSORS_PER_MODEL") \
   $([ -n "$BBS_MINING_CACHE_LOG_FILE" ] && echo "--log-file $BBS_MINING_CACHE_LOG_FILE") \
   $([ -n "$BBS_MINING_VERBOSE" ] && echo "$BBS_MINING_VERBOSE")
