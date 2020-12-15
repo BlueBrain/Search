dec384546a0226756f1574e0bf09df52cba1f340
Jan <jankrepl@yahoo.com>
2020-12-15 16:36:40 +0100

    [BBS-182] - Servers entrypoints using DVC to fetch latest models (#183)
    Co-authored-by: Jan Krepl <krepl@dgx1.bbp.epfl.ch>
    Co-authored-by: Francesco Casalegno <francesco.casalegno@gmail.com>

---
+++ docker/mining.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+source /src/docker/utils.sh
+ssh_setup
+dvc_pull_models
+
+# Launch mining server
+gunicorn --bind 0.0.0.0:8080 --workers 1 --timeout 7200 'bbsearch.entrypoints:get_mining_app()'
---
+++ docker/mining_cache.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+source /src/docker/utils.sh
+ssh_setup
+dvc_pull_models
+
+# Launch mining cache creation, using arguments only if defined
+create_mining_cache \
+  $([ -n "$BBS_MINING_CACHE_TARGET_TABLE" ] && echo "--target-table-name $BBS_MINING_CACHE_TARGET_TABLE") \
+  $([ -n "$BBS_MINING_CACHE_PROCESSORS_PER_MODEL" ] && echo "--n-processes-per-model $BBS_MINING_CACHE_PROCESSORS_PER_MODEL") \
+  $([ -n "$BBS_MINING_CACHE_LOG_FILE" ] && echo "--log-file $BBS_MINING_CACHE_LOG_FILE") \
+  $([ -n "$BBS_MINING_VERBOSE" ] && echo "$BBS_MINING_VERBOSE")
