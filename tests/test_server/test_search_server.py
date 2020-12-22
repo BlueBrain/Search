f922a59485117e92e6ec2e0fd80486afa129db3c
Stanislav Schmidt <stanislav.schmidt@epfl.ch>
2020-12-22 13:49:59 +0100

    Fix mypy in test_search_server

--- tests/test_server/test_search_server.py
+++ tests/test_server/test_search_server.py
@@ -46,22 +46,23 @@ def search_client(
 
 class TestSearchServer:
     def test_search_server(self, search_client):
+        # Test the help request
         response = search_client.post("/help")
         assert response.status_code == 200
         assert response.json["name"] == "SearchServer"
 
-        request_json = {"which_model": "BSV", "k": 3, "query_text": "hello"}
+        # Test a valid JSON request
+        k = 3
+        request_json = {"which_model": "BSV", "k": k, "query_text": "hello"}
         response = search_client.post("/", json=request_json)
         assert response.status_code == 200
-
         json_response = response.json
-        assert len(json_response["sentence_ids"]) == 3
-        assert len(json_response["similarities"]) == 3
+        assert len(json_response["sentence_ids"]) == k
+        assert len(json_response["similarities"]) == k
 
-        request_json = "data is not a json"
-        response = search_client.post("/", data=request_json)
+        # Test a non-JSON request
+        response = search_client.post("/", data="data is not a json")
         assert response.status_code == 200
-
         json_response = response.json
         assert json_response["sentence_ids"] is None
         assert json_response["similarities"] is None
