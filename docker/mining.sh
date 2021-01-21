314963acc4e5f15a5f5707b9b74b49aed6069434
Stanislav Schmidt <stanislav.schmidt@epfl.ch>
2021-01-21 15:38:46 +0100

    Replace entrypoints by entrypoint in Dockerfile

--- docker/mining.sh
+++ docker/mining.sh
@@ -22,4 +22,4 @@ ssh_setup
 dvc_pull_models
 
 # Launch mining server
-gunicorn --bind 0.0.0.0:8080 --workers 1 --timeout 7200 'bbsearch.entrypoints:get_mining_app()'
+gunicorn --bind 0.0.0.0:8080 --workers 1 --timeout 7200 'bbsearch.entrypoint:get_mining_app()'
