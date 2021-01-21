994245d8eaa901113c2bb43d1035cda1d325d8ee
Francesco Casalegno <francesco.casalegno@gmail.com>
2021-01-21 12:22:08 +0100

    [BBS-236] Add license + re-run DVC (#213)
    * Add license file from https://www.gnu.org/licenses/lgpl-3.0.md
    * Add license headers
    * Bump `bbs` version to `0.0.9` and run `dvc repro` on all pipelines

--- data_and_models/pipelines/sentence_embedding/eval.py
+++ data_and_models/pipelines/sentence_embedding/eval.py
@@ -1,3 +1,22 @@
+"""Evaluation of sentence embedding models."""
+
+# BBSearch is a text mining toolbox focused on scientific use cases.
+#
+# Copyright (C) 2020  Blue Brain Project, EPFL.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with this program. If not, see <https://www.gnu.org/licenses/>.
+
 import importlib
 import json
 import pathlib
@@ -38,6 +57,9 @@ args = parser.parse_args()
 
 
 def main():
+    import nltk
+    nltk.download("punkt")
+
     print("Reading params.yaml...")
     params = yaml.safe_load(open("params.yaml"))["eval"][args.model]
     module = importlib.import_module("bbsearch.embedding_models")
