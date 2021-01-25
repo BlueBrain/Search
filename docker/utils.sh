0b89501ab030e1e48ef66658d36a732287506904
Jan <jankrepl@yahoo.com>
2021-01-25 09:16:28 +0100

    [BBS 265] Untrack ee_models_library.csv from DVC (#216)

--- docker/utils.sh
+++ docker/utils.sh
@@ -30,7 +30,6 @@ dvc_pull_models() {
   dvc remote modify gpfs_ssh ask_password true
   dvc remote modify gpfs_ssh user $BBS_SSH_USERNAME
   pushd /src/data_and_models/pipelines/ner/ || exit
-  dvc pull ee_models_library.csv.dvc
   dvc pull $(< dvc.yaml grep -oE '\badd_er_[0-9]+\b' | xargs)
   popd || exit
 }
