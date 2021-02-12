ee0815b3915919a0b95cb3619c383063906b6802
Jan Krepl <jankrepl@yahoo.com>
2021-02-12 16:14:36 +0100

    Make Black happy [run travis]

--- tests/test_embedding_models.py
+++ tests/test_embedding_models.py
@@ -236,7 +236,6 @@ class TestEmbeddingModels:
         assert embedding.shape == ((768,) if n_sentences == 1 else (n_sentences, 768))
         senttrans_model.encode.assert_called_once()
 
-
     @pytest.mark.parametrize(
         "backend", ["TfidfVectorizer", "CountVectorizer", "HashingVectorizer"]
     )
