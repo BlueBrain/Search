ff77a071f998f59812ab9faf630f3f9ddf7d316b
Francesco Casalegno <francesco.casalegno@gmail.com>
2021-02-25 12:07:16 +0100

    remove img processing tag

--- setup.py
+++ setup.py
@@ -39,7 +39,6 @@ CLASSIFIERS = [
     "Programming Language :: Python :: 3.9",
     "Programming Language :: Python :: 3 :: Only",
     "Topic :: Scientific/Engineering :: Artificial Intelligence",
-    "Topic :: Scientific/Engineering :: Image Processing",
     "Topic :: Software Development :: Libraries :: Python Modules",
     "Topic :: Text Processing :: General",
 ]
