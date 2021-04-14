abe7f755bd151274b66d7bfcaf2430ebfcb45856
Francesco Casalegno <francesco.casalegno@gmail.com>
2021-04-14 12:00:34 +0200

    fix catalogue

--- setup.py
+++ setup.py
@@ -50,6 +50,7 @@ PYTHON_REQUIRES = ">=3.7"
 INSTALL_REQUIRES = [
     "Flask",
     "SQLAlchemy[mysql,pymysql]",
+    "catalogue==2.0.1",  # see https://github.com/explosion/catalogue/issues/17
     "cryptography",
     "h5py",
     "ipython",
