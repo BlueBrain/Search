b0597f35cca2b3f8f4a1c8bcfc0ce69ee09be8d4
FrancescoCasalegno <francesco.casalegno@gmail.com>
2021-04-30 17:16:00 +0200

    print info on weights diff

--- compare_all.sh
+++ compare_all.sh
@@ -7,7 +7,9 @@ do
 		MODEL2=repro_results_${device}_${transf}-transformer/exp_2/	
 		diff -qr $MODEL1 $MODEL2 
 		if [ ${transf} = "yes" ]; then
+			echo -n "model-best: "
 			python compare_torch.py ${MODEL1}/model-best/transformer/model/pytorch_model.bin ${MODEL2}/model-best/transformer/model/pytorch_model.bin
+			echo -n "model-last: "
 			python compare_torch.py ${MODEL1}/model-last/transformer/model/pytorch_model.bin ${MODEL2}/model-last/transformer/model/pytorch_model.bin
 		fi
 		echo " "
