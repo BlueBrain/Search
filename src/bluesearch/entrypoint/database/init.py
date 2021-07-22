5ecfaa15a07ade14c6027663eaf91df05545b794
Jan Krepl <jankrepl@yahoo.com>
2021-07-22 12:09:10 +0200

    Make the parser structure flexible (hopefully)

---
+++ src/bluesearch/entrypoint/database/init.py
@@ -0,0 +1,21 @@
+import argparse
+import sys
+from typing import List, Optional
+
+
+def get_parser() -> argparse.ArgumentParser:
+    parser = argparse.ArgumentParser(
+        description="Initialize.",
+    )
+    return parser
+
+
+def run() -> None:
+    print("Inside of the init command")
+
+
+if __name__ == "__main__":
+    parser = get_parser()
+    args = parser.parse_args()
+    run(**vars(args))
+
--- src/bluesearch/entrypoint/database/parent.py
+++ src/bluesearch/entrypoint/database/parent.py
@@ -2,26 +2,46 @@ import argparse
 import sys
 from typing import List, Optional
 
+from .add import get_parser as get_parser_add, run as run_add
+from .init import get_parser as get_parser_init, run as run_init
 
 def main(argv: Optional[List[str]] = None) -> int:
     parent_parser = argparse.ArgumentParser(description="Database management utilities")
 
     subparsers = parent_parser.add_subparsers(dest="command", required=True)
 
-    add_parser = subparsers.add_parser(
+    # Initialize subparsers
+    parser_add = get_parser_add()
+    parser_init = get_parser_init()
+
+    _ = subparsers.add_parser(
         "add",
-        help="Add entries.",
-        description="Add entries.",
+        description=parser_add.description,
+        help=parser_add.description,
+        parents=[parser_add],
+        add_help=False,
     )
-    init_parser = subparsers.add_parser(
+    _ = subparsers.add_parser(
         "init",
-        help="Initialize.",
-        description="Initialize.",
+        description=parser_init.description,
+        help=parser_init.description,
+        parents=[parser_init],
+        add_help=False,
     )
 
-    args = parent_parser.parse_args()
+    # Do parsing
+    args = parent_parser.parse_args(argv)
 
     print(args)
+    kwargs = vars(args)
+    command = kwargs.pop("command")
+
+    # Run logic
+    if command == "add":
+        run_add(**kwargs)
+    elif command == "init":
+        run_init(**kwargs)
+
     return 0
 
 if __name__ == "__main__":
