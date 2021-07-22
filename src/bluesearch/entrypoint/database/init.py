aa05435f2a333303316c39dac6f27e0e8475ad1f
Jan Krepl <jankrepl@yahoo.com>
2021-07-22 15:39:34 +0200

    Initialize dummy sqlite database

--- src/bluesearch/entrypoint/database/init.py
+++ src/bluesearch/entrypoint/database/init.py
@@ -2,16 +2,67 @@ import argparse
 import sys
 from typing import List, Optional
 
+import sqlalchemy
+
 
 def get_parser() -> argparse.ArgumentParser:
     parser = argparse.ArgumentParser(
         description="Initialize.",
     )
+    parser.add_argument(
+        "db_url",
+        type=str,
+        help="""
+        The location of the database depending on the database type.
+
+        For MySQL the server URL should be provided, for SQLite the
+        location of the database file. Generally, the scheme part of
+        the URL should be omitted, e.g. for MySQL the URL should be
+        of the form 'my_sql_server.ch:1234/my_database' and for SQLite
+        of the form '/path/to/the/local/database.db'.
+        """,
+    )
+    parser.add_argument(
+        "--db-type",
+        default="sqlite",
+        type=str,
+        choices=("mysql", "sqlite"),
+        help="Type of the database.",
+    )
     return parser
 
 
-def run() -> None:
-    print("Inside of the init command")
+def run(
+    *,
+    db_url: str,
+    db_type: str,
+) -> None:
+    """Initialize database."""
+    if db_type == "sqlite":
+        engine = sqlalchemy.create_engine(f"sqlite:///{db_url}")
+
+    elif db_type == "mysql":
+        raise NotImplementedError
+
+    else:
+        raise ValueError(f"Unrecognize database type {db_type}")
+
+
+    metadata = sqlalchemy.MetaData()
+
+    # Creation of the schema of the tables
+    sqlalchemy.Table(
+        "articles",
+        metadata,
+        sqlalchemy.Column(
+            "article_id", sqlalchemy.Integer(), primary_key=True, autoincrement=True
+        ),
+        sqlalchemy.Column("title", sqlalchemy.Text()),
+    )
+
+    # Construction
+    with engine.begin() as connection:
+        metadata.create_all(connection)
 
 
 if __name__ == "__main__":
