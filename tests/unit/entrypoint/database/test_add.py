27306bd7ab916e4270f6da99088f5ffd2ca890b6
Pierre-Alexandre Fonta <pierre-alexandre.fonta@epfl.ch>
2021-09-29 09:50:33 +0200

    Fix uncovered issue in test_add.py

--- tests/unit/entrypoint/database/test_add.py
+++ tests/unit/entrypoint/database/test_add.py
@@ -33,9 +33,9 @@ def test_sqlite_cord19(engine_sqlite, tmp_path):
     articles = [
         Article(
             title=f"title_{i}",
-            authors=[f"author_{i}"],
-            abstract=[f"abstract_{i}"],
-            section_paragraphs=[("Conclusion", f"conclusion_{i}")],
+            authors=(f"author_{i}",),
+            abstract=(f"abstract_{i}",),
+            section_paragraphs=(("Conclusion", f"conclusion_{i}"),),
             uid=f"uid_{i}",
             pubmed_id=f"pubmed_id_{i}",
             pmc_id=f"pmc_id_{i}",
