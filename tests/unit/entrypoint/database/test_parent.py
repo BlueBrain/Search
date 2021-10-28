537c6497d21fe356b637544cc6544c758ac7a1e1
Jan Krepl <jankrepl@yahoo.com>
2021-10-28 18:26:06 +0200

    Test help of all subcommands

--- tests/unit/entrypoint/database/test_parent.py
+++ tests/unit/entrypoint/database/test_parent.py
@@ -3,6 +3,6 @@ import subprocess
 import pytest
 
 
-@pytest.mark.parametrize("command", ["add", "init"])
+@pytest.mark.parametrize("command", ["add", "convert-pdf", "init", "parse"])
 def test_commands_work(command):
     subprocess.check_call(["bbs_database", command, "--help"])
