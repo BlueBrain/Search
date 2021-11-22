14ce609a070995f961c49cc5d002b57c1687b9bf
Stanislav Schmidt <stanislav.schmidt@epfl.ch>
2021-11-22 15:39:55 +0100

    Write tests for grobid_is_alive

--- tests/unit/database/test_pdf.py
+++ tests/unit/database/test_pdf.py
@@ -1,6 +1,10 @@
+import re
+
+import pytest
+import requests
 import responses
 
-from bluesearch.database.pdf import grobid_pdf_to_tei_xml
+from bluesearch.database.pdf import grobid_is_alive, grobid_pdf_to_tei_xml
 
 
 @responses.activate
@@ -16,3 +20,24 @@ def test_conversion_pdf(monkeypatch):
     result = grobid_pdf_to_tei_xml(b"", host="fake_host", port=8888)
     assert result == "body"
     assert len(responses.calls) == 1
+
+
+@pytest.mark.parametrize(
+    ("body", "expected_result"),
+    (
+        ("true", True),
+        (requests.RequestException(), False),
+        ("false", False),
+        ("unknown", False),
+    ),
+)
+@responses.activate
+def test_grobid_is_alive(body, expected_result):
+    host = "host"
+    port = 12345
+    responses.add(
+        responses.GET,
+        re.compile(fr"http://{host}:{port}/.*"),
+        body=body,
+    )
+    assert grobid_is_alive(host, port) is expected_result
