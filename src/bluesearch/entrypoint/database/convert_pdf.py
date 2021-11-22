0684fe6bd4c4f73ecc722bf48fcc4c58687ac3b0
Stanislav Schmidt <stanislav.schmidt@epfl.ch>
2021-11-22 17:36:29 +0100

    Chagne the covert-pdf subcommand to process many files

--- src/bluesearch/entrypoint/database/convert_pdf.py
+++ src/bluesearch/entrypoint/database/convert_pdf.py
@@ -21,6 +21,9 @@ import argparse
 import logging
 import pathlib
 import textwrap
+from typing import Iterable
+
+from bluesearch.database.pdf import grobid_is_alive, grobid_pdf_to_tei_xml
 
 logger = logging.getLogger(__name__)
 
@@ -63,22 +66,31 @@ def init_parser(parser: argparse.ArgumentParser) -> argparse.ArgumentParser:
         help="The port of the GROBID server.",
     )
     parser.add_argument(
-        "input_pdf_path",
+        "input_path",
         type=pathlib.Path,
-        metavar="INPUT-PDF-PATH",
-        help="The path of the input PDF file.",
+        metavar="INPUT-PATH",
+        help="The path to a single PDF file or a directory with many PDF files.",
     )
     parser.add_argument(
-        "output_xml_path",
+        "-o",
+        "--output-dir",
         type=pathlib.Path,
-        metavar="OUTPUT-XML-PATH",
-        help="The path of the output XML file.",
+        metavar="OUTPUT-DIR",
+        help="""
+        The output directory where the XML file(s) will be saved. If not
+        provided the output files will be placed in the same directory as
+        the input files.
+        """,
     )
     parser.add_argument(
         "--force",
         "-f",
         action="store_true",
-        help="Overwrite the output file if it already exits.",
+        help="""
+        Overwrite the output files if they already exits. Without this flag
+        all PDF files for which the output XML file already exists will
+        be skipped
+        """,
     )
 
     return parser
@@ -87,8 +99,8 @@ def init_parser(parser: argparse.ArgumentParser) -> argparse.ArgumentParser:
 def run(
     grobid_host: str,
     grobid_port: int,
-    input_pdf_path: pathlib.Path,
-    output_xml_path: pathlib.Path,
+    input_path: pathlib.Path,
+    output_dir: pathlib.Path | None,
     *,
     force: bool,
 ) -> int:
@@ -112,10 +124,10 @@ def run(
         The host of the GROBID service.
     grobid_port
         The port of the GROBID service.
-    input_pdf_path
-        The path to the input PDF file.
-    output_xml_path
-        The path to the output XML file.
+    input_path
+        The path to the input PDF file or a directory with PDF files.
+    output_dir
+        The output directory for the XML files.
     force
         If true overwrite the output file if it already exists.
 
@@ -124,49 +136,99 @@ def run(
     int
         The exit code of the command
     """
-    logger.info("Initialising")
-    from bluesearch.database.pdf import grobid_is_alive, grobid_pdf_to_tei_xml
-
     # Check the GROBID server
     if not grobid_is_alive(grobid_host, grobid_port):
-        logger.error(
-            "The GROBID server is not alive; please check your connection "
-            "and server configuration"
-        )
+        logger.error("The GROBID server is not alive")
         return 1
-    else:
-        logger.info("The GROBID server is alive")
 
     # Check if the input file exists
-    if not input_pdf_path.exists():
-        logger.error(
-            f"The input file {str(input_pdf_path)!r} does not exist.",
-        )
+    if not input_path.exists():
+        logger.error(f"The input path {str(input_path)!r} does not exist")
         return 1
 
-    # Check if the output file already exists
-    if output_xml_path.exists() and not force:
-        logger.error(
-            f"The output file {str(output_xml_path)!r} already exists. "
-            "Either delete it or use the --force option to overwrite it.",
-        )
-        return 1
+    # Collect input paths
+    input_paths: Iterable[pathlib.Path]
+    if input_path.is_file():
+        input_paths = [input_path]
+    else:
+        input_paths = _keep_pdfs_only(input_path.iterdir())
 
-    # Read the PDF file
-    logger.info("Reading the PDF file")
-    with input_pdf_path.open("rb") as fh_pdf:
-        pdf_content = fh_pdf.read()
-
-    # Convert the PDF to XML
-    logger.info("Converting PDF to XML")
-    xml_content = grobid_pdf_to_tei_xml(pdf_content, grobid_host, grobid_port)
-
-    # Write the XML file
-    logger.info("Writing the XML file to disk")
-    with output_xml_path.open("w") as fh_xml:
-        n_bytes = fh_xml.write(xml_content)
-    logger.info("Wrote %d bytes to %s", n_bytes, output_xml_path.resolve().as_uri())
-
-    logger.info("PDF conversion done")
+    # Convert
+    for input_path in input_paths:
+        _convert_pdf_file(grobid_host, grobid_port, input_path, output_dir, force)
 
     return 0
+
+
+def _keep_pdfs_only(pdf_paths: Iterable[pathlib.Path]) -> list[pathlib.Path]:
+    """Filter a collection of paths and paths to PDF files only.
+
+    Parameters
+    ----------
+    pdf_paths
+        An iterable of paths.
+
+    Returns
+    -------
+    A list of paths that are files and have the PDF file extension.
+    """
+    filtered_paths = []
+    for path in pdf_paths:
+        if not path.is_file():
+            logger.info("Will skip directory %s", path.resolve().as_uri())
+            continue
+        if path.suffix.lower() != ".pdf":
+            logger.info("Will skip non-PDF file %s", path.resolve().as_uri())
+            continue
+        filtered_paths.append(path)
+
+    return filtered_paths
+
+
+def _convert_pdf_file(
+    grobid_host: str,
+    grobid_port: int,
+    input_path: pathlib.Path,
+    output_dir: pathlib.Path | None,
+    force: bool,
+) -> None:
+    """Convert a single PDF file to XML and write the XML file to disk.
+
+    Parameters
+    ----------
+    grobid_host
+        The host of the GROBID service.
+    grobid_port
+        The port of the GROBID service.
+    input_path
+        The path to the input PDF file.
+    output_dir
+        The output directory for the XML file.
+    force
+        If true overwrite the output file if it already exists. If false and
+        the output file already exists then do nothing.
+    """
+    output_name = input_path.with_suffix(".xml").name
+    if output_dir is None:
+        output_path = input_path.parent / output_name
+    else:
+        output_path = output_dir / output_name
+
+    if output_path.exists() and not force:
+        logger.info(
+            "Not overwriting existing file %s, use --force to always overwrite.",
+            output_path.resolve().as_uri(),
+        )
+        return
+
+    logger.info("Reading %s", input_path.resolve().as_uri())
+    with input_path.open("rb") as fh_pdf:
+        pdf_content = fh_pdf.read()
+
+    logger.info("Converting %s to XML", input_path.resolve().as_uri())
+    xml_content = grobid_pdf_to_tei_xml(pdf_content, grobid_host, grobid_port)
+
+    logger.info("Writing %s to disk", output_path.resolve().as_uri())
+    with output_path.open("w") as fh_xml:
+        n_bytes = fh_xml.write(xml_content)
+    logger.info("Wrote %d bytes", n_bytes)
