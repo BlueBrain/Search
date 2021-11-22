ea428566148c26affedcd5fe712330cea9cbf757
Stanislav Schmidt <stanislav.schmidt@epfl.ch>
2021-11-22 17:56:06 +0100

    Adjust tests for convert-pdf

--- tests/unit/entrypoint/database/test_convert_pdf.py
+++ tests/unit/entrypoint/database/test_convert_pdf.py
@@ -10,8 +10,8 @@ from bluesearch.entrypoint.database import convert_pdf
 CONVERT_PDF_PARAMS = {
     "grobid_host",
     "grobid_port",
-    "input_pdf_path",
-    "output_xml_path",
+    "input_path",
+    "output_dir",
     "force",
 }
 
@@ -19,16 +19,14 @@ CONVERT_PDF_PARAMS = {
 def test_init_parser():
     parser = convert_pdf.init_parser(argparse.ArgumentParser())
 
-    args = parser.parse_args(
-        ["my-host", "1234", "/path/to/file.pdf", "/path/to/file.xml"]
-    )
+    args = parser.parse_args(["my-host", "1234", "/input/path"])
     assert vars(args).keys() == CONVERT_PDF_PARAMS
 
     # Test the values
     assert args.grobid_host == "my-host"
     assert args.grobid_port == 1234
-    assert args.input_pdf_path == pathlib.Path("/path/to/file.pdf")
-    assert args.output_xml_path == pathlib.Path("/path/to/file.xml")
+    assert args.input_path == pathlib.Path("/input/path")
+    assert args.output_dir is None
     assert args.force is False
 
 
@@ -44,8 +42,10 @@ class TestRun:
             (False, 1),
         ),
     )
-    @unittest.mock.patch("bluesearch.database.pdf.grobid_pdf_to_tei_xml")
-    @unittest.mock.patch("bluesearch.database.pdf.grobid_is_alive")
+    @unittest.mock.patch(
+        "bluesearch.entrypoint.database.convert_pdf.grobid_pdf_to_tei_xml"
+    )
+    @unittest.mock.patch("bluesearch.entrypoint.database.convert_pdf.grobid_is_alive")
     def test_is_alive_check_works(
         self,
         grobid_is_alive,
@@ -61,19 +61,18 @@ class TestRun:
         # Configure file paths
         input_pdf_file = tmp_path / "my-file.pdf"
         input_pdf_file.touch()
-        output_xml_file = tmp_path / "my-file.xml"
 
         # Run the test
         exit_code = convert_pdf.run(
             "host",
             1234,
             input_pdf_file,
-            output_xml_file,
+            None,
             force=False,
         )
         assert exit_code == expected_exit_code
 
-    @unittest.mock.patch("bluesearch.database.pdf.grobid_is_alive")
+    @unittest.mock.patch("bluesearch.entrypoint.database.convert_pdf.grobid_is_alive")
     def test_nonexistent_pdf_errors(self, grobid_is_alive):
         grobid_is_alive.return_value = True
 
@@ -86,22 +85,10 @@ class TestRun:
         )
         assert exit_code == 1
 
-    @unittest.mock.patch("bluesearch.database.pdf.grobid_is_alive")
-    def test_output_file_exists_errors(self, grobid_is_alive, tmp_path):
-        grobid_is_alive.return_value = True
-
-        input_pdf_file = tmp_path / "my-file.pdf"
-        input_pdf_file.touch()
-        output_xml_file = tmp_path / "my-file.xml"
-        output_xml_file.touch()
-
-        exit_code = convert_pdf.run(
-            "host", 1234, input_pdf_file, output_xml_file, force=False
-        )
-        assert exit_code == 1
-
-    @unittest.mock.patch("bluesearch.database.pdf.grobid_pdf_to_tei_xml")
-    @unittest.mock.patch("bluesearch.database.pdf.grobid_is_alive")
+    @unittest.mock.patch(
+        "bluesearch.entrypoint.database.convert_pdf.grobid_pdf_to_tei_xml"
+    )
+    @unittest.mock.patch("bluesearch.entrypoint.database.convert_pdf.grobid_is_alive")
     def test_pdf_conversion_works(
         self, grobid_is_alive, grobid_pdf_to_tei_xml, tmp_path
     ):
@@ -119,9 +106,7 @@ class TestRun:
         grobid_pdf_to_tei_xml.return_value = "<xml>parsed</xml>"
 
         # Call the entry point
-        exit_code = convert_pdf.run(
-            "host", 1234, input_pdf_file, output_xml_file, force=False
-        )
+        exit_code = convert_pdf.run("host", 1234, input_pdf_file, None, force=False)
 
         # Checks
         assert exit_code == 0
