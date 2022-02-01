66f836a4de958b76bb76dc7257d3a70bfc30b74d
Stanislav Schmidt <stanislav.schmidt@epfl.ch>
2022-02-01 08:46:19 +0000

    Add a missing __future__ import

--- src/bluesearch/entrypoint/database/parse_mesh_rdf.py
+++ src/bluesearch/entrypoint/database/parse_mesh_rdf.py
@@ -15,6 +15,8 @@
 #  You should have received a copy of the GNU Lesser General Public License
 #  along with this program. If not, see <https://www.gnu.org/licenses/>.
 """CLI sub-command for parsing MeSH RDF files."""
+from __future__ import annotations
+
 import argparse
 import collections
 import gzip
