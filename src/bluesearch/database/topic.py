3465371a0f66e761a7c88427c8b521057bb0ba19
Emilie Delattre <emilie.delattre@epfl.ch>
2022-02-14 17:15:42 +0100

    Update JATSXML depending on the extension

--- src/bluesearch/database/topic.py
+++ src/bluesearch/database/topic.py
@@ -276,10 +276,16 @@ def get_topics_for_pmc_article(
         Journal topics for the given article.
     """
     # Determine journal title
-    if str(pmc_path).endswith(".xml"):
+    *_, extension = str(pmc_path).lower().rpartition(".")
+    if extension == "xml":
         parser = JATSXMLParser.from_xml(pmc_path)
-    else:
+    elif extension == "meca" or extension == "zip":
         parser = JATSXMLParser.from_zip(pmc_path)
+    else:
+        raise ValueError(
+            f"Unknown file extension for JATS XML: {extension!r}. Only XML and ZIP "
+            f"are supported. File: {str(pmc_path)!r}"
+        )
 
     nlm_ta = parser.content.find(
         "./front/journal-meta/journal-id[@journal-id-type='nlm-ta']"
