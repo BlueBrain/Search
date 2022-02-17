f629ae1af7ce14015d543b3d554838b0d4620447
Jan Krepl <jankrepl@yahoo.com>
2022-02-17 17:01:16 +0100

    Fix the immortal bug

--- src/bluesearch/entrypoint/database/topic_extract.py
+++ src/bluesearch/entrypoint/database/topic_extract.py
@@ -188,7 +188,7 @@ def run(
         mesh_tree = mesh.MeSHTree.load(mesh_topic_db)
         for path in inputs:
             logger.info(f"Processing {path}")
-            with gzip.open(input_path) as xml_stream:
+            with gzip.open(path) as xml_stream:
                 articles = ElementTree.parse(xml_stream)
 
             for i, article in enumerate(articles.iter("PubmedArticle")):
