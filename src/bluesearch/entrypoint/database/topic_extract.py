c9dcb395bac93fb4b78b258278c131bfb149ff27
Jan Krepl <jankrepl@yahoo.com>
2022-02-23 21:58:38 +0100

    Add separate try except blocks for each source

--- src/bluesearch/entrypoint/database/topic_extract.py
+++ src/bluesearch/entrypoint/database/topic_extract.py
@@ -173,7 +173,11 @@ def run(
         for path in inputs:
             logger.info(f"Processing {path}")
             topic_info = TopicInfo(source=article_source, path=path.resolve())
-            journal_topics = get_topics_for_pmc_article(path)
+            try:
+                journal_topics = get_topics_for_pmc_article(path)
+            except Exception:
+                logger.error(f"Failed to extract topic from {path}")
+
             if journal_topics:
                 topic_info.add_journal_topics(
                     "MeSH", mesh.resolve_parents(journal_topics, mesh_tree)
@@ -198,8 +202,12 @@ def run(
                     path=path.resolve(),
                     element_in_file=i,
                 )
-                article_topics = extract_article_topics_for_pubmed_article(article)
-                journal_topics = extract_journal_topics_for_pubmed_article(article)
+                try:
+                    article_topics = extract_article_topics_for_pubmed_article(article)
+                    journal_topics = extract_journal_topics_for_pubmed_article(article)
+                except Exception:
+                    logger.error(f"Failed to extract topic from {i}")
+
                 if article_topics:
                     topic_info.add_article_topics(
                         "MeSH", mesh.resolve_parents(article_topics, mesh_tree)
@@ -212,7 +220,11 @@ def run(
     elif article_source is ArticleSource.ARXIV:
         for path, article_topics in get_topics_for_arxiv_articles(inputs).items():
             topic_info = TopicInfo(source=article_source, path=path)
-            topic_info.add_article_topics("arXiv", article_topics)
+            try:
+                topic_info.add_article_topics("arXiv", article_topics)
+            except Exception:
+                logger.error(f"Failed to extract topic from {path}")
+
             all_results.append(topic_info.json())
     elif article_source in {ArticleSource.BIORXIV, ArticleSource.MEDRXIV}:
         for path in inputs:
@@ -220,7 +232,11 @@ def run(
             topic, journal = extract_article_topics_from_medrxiv_article(path)
             journal = journal.lower()
             topic_info = TopicInfo(source=ArticleSource(journal), path=path)
-            topic_info.add_article_topics("Subject Area", [topic])
+            try:
+                topic_info.add_article_topics("Subject Area", [topic])
+            except Exception:
+                logger.error(f"Failed to extract topic from {path}")
+
             all_results.append(topic_info.json())
     else:
         logger.error(f"The source type {source!r} is not implemented yet")
