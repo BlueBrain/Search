17b56d7322fbf0e45b5119d0cafc91b2745c9d30
Diogo Santos <diogo.reissantos@epfl.ch>
2022-09-22 11:13:48 +0200

    linters

--- tests/unit/k8s/test_create_indices.py
+++ tests/unit/k8s/test_create_indices.py
@@ -3,8 +3,8 @@ import pytest
 from bluesearch.k8s.connect import connect
 from bluesearch.k8s.create_indices import add_index, remove_index
 
-ES_URL="http://localhost:9200"
-ES_PASS=""
+ES_URL = "http://localhost:9200"
+ES_PASS = ""
 
 
 @pytest.fixture()
@@ -19,13 +19,14 @@ def get_es_client(monkeypatch):
 
     yield client
 
+
 def test_create_and_remove_index(get_es_client):
     client = get_es_client
 
     if client is None:
         pytest.skip("Elastic search is not available")
 
-    index="test_index"
+    index = "test_index"
 
     add_index(client, index)
     remove_index(client, index)
