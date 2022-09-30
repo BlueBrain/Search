0c0b1d3fcbe064afd53069f53b02bbde0b62d455
Diogo Santos <diogo.reissantos@epfl.ch>
2022-09-30 11:56:30 +0200

    add seldon embedding

--- src/bluesearch/k8s/embeddings.py
+++ src/bluesearch/k8s/embeddings.py
@@ -17,22 +17,31 @@
 """Embed the paragraphs in the database."""
 from __future__ import annotations
 
+import functools
 import logging
+import os
 
 import elasticsearch
 import numpy as np
+import requests
 import tqdm
+from dotenv import load_dotenv
 from elasticsearch.helpers import scan
 
 from bluesearch.embedding_models import SentTransformer
 
+load_dotenv()
+
 logger = logging.getLogger(__name__)
 
 
-def embed_locally(
+def embed(
     client: elasticsearch.Elasticsearch,
-    model_name: str = "sentence-transformers/multi-qa-MiniLM-L6-cos-v1",
     index: str = "paragraphs",
+    embedding_method: str = "seldon",
+    model_name: str = "minilm",
+    namespace: str = "seldon",
+    polling: str = "mean",
 ) -> None:
     """Embed the paragraphs in the database locally.
 
@@ -40,10 +49,26 @@ def embed_locally(
     ----------
     client
         Elasticsearch client.
+    index
+        Name of the ES index.
+    embedding_method
+        Method to use to embed the paragraphs.
     model_name
         Name of the model to use for the embedding.
+    namespace
+        Namespace of the Seldon deployment.
+    polling
+        Polling method to use for the Seldon deployment.
     """
-    model = SentTransformer(model_name)
+    if embedding_method == "seldon":
+        embed = functools.partial(
+            _embed_seldon, namespace=namespace, model_name=model_name, polling=polling
+        )
+    elif embedding_method == "local":
+        embed = functools.partial(
+            _embed_locally,
+            model_name=model_name,
+        )
 
     # get paragraphs without embeddings
     query = {"bool": {"must_not": {"exists": {"field": "embedding"}}}}
@@ -58,8 +83,98 @@ def embed_locally(
         desc="Updating embeddings",
     )
     for hit in scan(client, query={"query": query}, index=index):
-        emb = model.embed(hit["_source"]["text"])
-        if not model.normalized:
-            emb /= np.linalg.norm(emb)
-        client.update(index=index, doc={"embedding": emb.tolist()}, id=hit["_id"])
+        emb = embed(hit["_source"]["text"])
+        client.update(index=index, doc={"embedding": emb}, id=hit["_id"])
         progress.update(1)
+
+
+def _embed_locally(
+    text: str, model_name: str = "sentence-transformers/multi-qa-MiniLM-L6-cos-v1"
+) -> list[float]:
+    """Embed the paragraphs in the database locally.
+
+    Parameters
+    ----------
+    text
+        Text to embed.
+    model_name
+        Name of the model to use for the embedding.
+
+    Returns
+    -------
+    embedding
+        Embedding of the text.
+    """
+    model = SentTransformer(model_name)
+    emb = model.embed(text)
+    if not model.normalized:
+        emb /= np.linalg.norm(emb)
+    return emb.tolist()
+
+
+def _embed_seldon(
+    text: str,
+    namespace: str = "seldon",
+    model_name: str = "minilm",
+    polling: str = "mean",
+) -> list[float]:
+    """Embed the paragraphs in the database using Seldon.
+
+    Parameters
+    ----------
+    text
+        Text to embed.
+    namespace
+        Namespace of the Seldon deployment.
+    model_name
+        Name of the Seldon deployment.
+    polling
+        Polling method to use for the Seldon deployment.
+
+    Returns
+    -------
+    embedding
+        Embedding of the text.
+    """
+    url = (
+        "http://"
+        + os.environ["SELDON_URL"]
+        + "/seldon/"
+        + namespace
+        + "/"
+        + model_name
+        + "/v2/models/transformer/infer"
+    )
+
+    # create payload
+    response = requests.post(
+        url,
+        json={
+            "inputs": [
+                {
+                    "name": "args",
+                    "shape": [1],
+                    "datatype": "BYTES",
+                    "data": text,
+                    "parameters": {"content_type": "str"},
+                }
+            ]
+        },
+    )
+
+    # convert the response to a numpy array
+    tensor = response.json()["outputs"][0]["data"][0]
+    tensor = tensor[3:-3].split("], [")
+    tensor = np.vstack([np.array(t.split(", "), dtype=np.float32) for t in tensor])
+
+    # apply the polling method
+    if polling:
+        if polling == "max":
+            tensor = np.max(tensor, axis=0)
+        elif polling == "mean":
+            tensor = np.mean(tensor, axis=0)
+
+    # normalize the embedding
+    tensor /= np.linalg.norm(tensor)
+
+    return tensor.tolist()
