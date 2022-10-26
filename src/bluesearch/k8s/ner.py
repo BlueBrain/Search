e1017aa2f97fc2f8c018faf0cadb010ad54598e3
Emilie Delattre <emilie.delattre@epfl.ch>
2022-10-26 17:19:10 +0200

    Add test for run_ner_model_remote

--- src/bluesearch/k8s/ner.py
+++ src/bluesearch/k8s/ner.py
@@ -15,6 +15,8 @@
 # You should have received a copy of the GNU Lesser General Public License
 # along with this program. If not, see <https://www.gnu.org/licenses/>.
 """Perform Name Entity Recognition (NER) on paragraphs."""
+from __future__ import annotations
+
 import logging
 import os
 from typing import Any
